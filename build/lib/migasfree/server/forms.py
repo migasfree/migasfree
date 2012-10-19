367a0030268abf9989af46ed3ff209a3cc510063
Alberto Gacías <alberto@migasfree.org>
2012-10-19 17:22:29 +0200

    deleted build/

--- build/lib/migasfree/server/forms.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django import forms
-
-class ParametersForm(forms.Form):
-    id_query = forms.CharField(required=True, widget=forms.HiddenInput())
-    user_version = forms.CharField(required=True, widget=forms.HiddenInput())
