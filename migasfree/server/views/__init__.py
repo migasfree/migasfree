894c59807deee4551d3d7116c49ea35772920c51
Jose Antonio Chavarría <jachavar@gmail.com>
2012-12-05 14:47:28 +0100

    Deleted deprecated views: chart_selection, query_selection

--- migasfree/server/views/__init__.py
+++ migasfree/server/views/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from migasfree.server.views.stats import chart, chart_selection, \
+from migasfree.server.views.stats import chart, \
     daily_updated, delay_schedule, hourly_updated, monthly_updated, \
     version_computer
-from migasfree.server.views.queries import query, query_selection, \
+from migasfree.server.views.queries import query, \
     query_message, query_message_server
 from migasfree.server.views.hardware import hardware, hardware_resume, \
     load_hw, process_hw
