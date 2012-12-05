c69074900c8a356d70db3ceff5fa2766df1b0608
Jose Antonio Chavarría <jachavar@gmail.com>
2012-12-05 14:57:14 +0100

    Datum to include in all requests

---
+++ migasfree/server/context_processors.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from migasfree.server.models import get_version_names, get_query_names, \
+    UserProfile
+
+
+def query_names(request):
+    return {'query_names': get_query_names()}
+
+
+def version_names(request):
+    try:
+        current = UserProfile.objects.get(id=request.user.id).version.name
+    except:
+        current = ''
+
+    return {
+        'version_names': get_version_names(),
+        'current_version': current
+    }
