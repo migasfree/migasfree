ae979a60ded67b1368981aee9b6a184f3c8c51dc
Jose Antonio Chavarría <jachavar@gmail.com>
2012-12-07 17:03:30 +0100

    Added ajax views (ajax_status, ajax_status_list)

--- migasfree/server/views/dashboard.py
+++ migasfree/server/views/dashboard.py
@@ -7,52 +7,62 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.db.models import Q
+from django.http import HttpResponse
 
-from migasfree.server.models import Checking, Message
+from migasfree.server.models import *
+from migasfree.settings import STATIC_URL
+
+
+def execute_active_checkings():
+    """
+    Returns active checkings results, as a list
+    If an error occurs, returns a dictionary with execution error information
+    If no results, returns empty list
+    """
+    status = []
+    for check in Checking.objects.filter(active=True):
+        try:
+            exec(check.code.replace("\r", ""))
+        except:
+            return {'error': {
+                'description': "Error in field 'code' of Checking: %s"
+                    % check.name,
+                'contentpage': '%s\n%s' % (check.code, str(sys.exc_info()))
+            }}
+
+        result = vars().get('result', 0)
+        if result > 0:
+            status.append({
+                'icon': vars().get('icon', 'information.png'),
+                'url': vars().get('url', reverse('dashboard')),
+                'result': '%d %s' % (result, vars().get('msg', check.name))
+            })
+
+    return status
 
 
 @login_required
 def main(request):
     """
-    dashboard of migasfree
+    Status of checkings
     """
-
-    status = []
-    for obj in Checking.objects.filter(active=True):
-        msg = ""
-        try:
-            exec(obj.code.replace("\r", ""))
-            result = vars().get('result', 0)
-            if result != 0:
-                msg = _(vars().get('msg', obj.name))
-                url = vars().get('url', reverse('bootstrap'))
-                icon = vars().get('icon', 'information.png')
-
-                status.append(
-                    {
-                        'icon': icon,
-                        'url': url,
-                        'result': '%s %s' % (str(result), msg)
-                    }
-                )
-        except:
-            return render(
-                request,
-                'error.html',
-                {
-                    'description': "Error in field 'code' of Checking:",
-                    'contentpage': "%s\n%s" % (msg, str(sys.exc_info()))
-                }
-            )
-
-    if len(status) == 0:
-        status.append(
+    status = execute_active_checkings()
+    if type(status) is dict and status.get('error').get('description'):
+        return render(
+            request,
+            'error.html',
             {
-                'icon': 'checking.png',
-                'url': '#',
-                'result': _('All O.K.')
+                'description': status.get('error').get('description'),
+                'contentpage': status.get('error').get('contentpage')
             }
         )
+
+    if len(status) == 0:
+        status.append({
+            'icon': 'checking.png',
+            'url': '#',
+            'result': _('All O.K.')
+        })
 
     return render(
         request,
@@ -62,3 +72,53 @@ def main(request):
             'status': status,
         }
     )
+
+
+def ajax_status(request):
+    ret = '%s: ' % _('Status')
+
+    status = execute_active_checkings()
+    if type(status) is dict:
+        ret += '<a href="%s">%s</a>' % (reverse('dashboard'), _('Error'))
+    elif len(status) == 0:
+        ret += '<strong>%s</strong>' % _('All O.K.')
+    else:
+        ret += '<a href="%s">%s</a>' % (reverse('dashboard'), _('Warning'))
+
+    return HttpResponse(ret, content_type='text/plain')
+
+
+def ajax_status_list(request):
+    ret = ''
+    status = execute_active_checkings()
+    if type(status) is dict and status.get('error').get('description'):
+        return render(
+            request,
+            'error.html',
+            {
+                'description': status.get('error').get('description'),
+                'contentpage': status.get('error').get('contentpage')
+            }
+        )
+
+    if len(status) == 0:
+        ret += '<li><img src="%sicons/checking.png" />%s</li>' % (
+            STATIC_URL,
+            _('All O.K.')
+        )
+    else:
+        css_class = 'odd'
+        for item in status:
+            ret += '<li class="%s"><a href="%s"><img src="%sicons/%s" /> %s</a></li>' % (
+                css_class,
+                item['url'],
+                STATIC_URL,
+                item['icon'],
+                item['result']
+            )
+            if css_class == 'odd':
+                css_class = 'even'
+            else:
+                css_class = 'odd'
+
+    return HttpResponse(ret, content_type='text/plain')
