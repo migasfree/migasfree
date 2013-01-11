0f04228198894ad011115e11b3472a3f895902c2
Jose Antonio Chavarría <jachavar@gmail.com>
2013-01-11 22:02:25 +0100

    New create_repos function in repository

--- migasfree/server/views/__init__.py
+++ migasfree/server/views/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# import order is very important!!!
+
 from migasfree.server.views.stats import chart, \
     daily_updated, delay_schedule, hourly_updated, monthly_updated, \
     version_computer
@@ -8,9 +10,9 @@ from migasfree.server.views.queries import query, \
 from migasfree.server.views.hardware import hardware, hardware_resume, \
     load_hw, process_hw
 from migasfree.server.views.devices import device, load_devices
+from migasfree.server.views.repository import create_repositories, create_repos
 from migasfree.server.views.client_api import api
 from migasfree.server.views.packages import change_version, info
 from migasfree.server.views.dashboard import status, ajax_status, \
     get_current_status
 from migasfree.server.views.login import login
-from migasfree.server.views.repository import createrepositories
