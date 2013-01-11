28c66772da81e73822f6d1390ddaccaf19741b41
Jose Antonio Chavarría <jachavar@gmail.com>
2013-01-11 21:54:11 +0100

    Use of urlresolvers.reverse for URLs
    Changed assertEquals to assertEqual
    
    Changed failUnlessEqual to assertEqual (deprecated warning)

--- migasfree/server/tests.py
+++ migasfree/server/tests.py
@@ -9,17 +9,19 @@
 import os
 
 from datetime import datetime
+
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 #from django.test.client import Client
 
 from migasfree.server.models import *
-
 from migasfree.settings import MIGASFREE_DB_DIR
+
 
 class RepositoryTestCase(TestCase):
     fixtures = [os.path.join(MIGASFREE_DB_DIR, 'test.json'), ]
 
-    def setUp(self):  #pylint: disable-msg=C0103
+    def setUp(self):  # pylint: disable-msg=C0103
         self.test1 = Repository()
         self.test1.name = "TEST 1 2"
         self.test1.active = True
@@ -29,26 +31,26 @@ class RepositoryTestCase(TestCase):
         self.test1.toinstall = "bluefish"
         self.test1.toremove = ""
         self.test1.save()
-        #self.test1.packages.add(oPackage)
-        #self.test1.attributes.add(Attribute.objects.get(value="RYS.AYTOZAR"))
-        self.test1.save()
 
 # WARNING: the following methods must start with "test"
     def test_repository_name(self):
-        self.assertEquals(self.test1.name, 'TEST_1_2')
+        self.assertEqual(self.test1.name, 'TEST_1_2')
 
     def test_login_site(self):
         result = self.client.login(username='admin', password='admin')
-        self.assertEquals(result, True)
-        response = self.client.get('/migasfree/admin/')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(result, True)
 
-        # Issue a GET request.
-        response = self.client.get('/migasfree/admin/server/repository/')
+        response = self.client.get(reverse('admin:index'))
+        self.assertEqual(response.status_code, 200)
 
-        # Check that the response is 200 OK.
-        self.failUnlessEqual(response.status_code, 200)
+        response = self.client.get(
+            reverse('admin:server_repository_changelist')
+        )
+        self.assertEqual(response.status_code, 200)
 
-#        # Check that the rendered context contains 2 repositories.
-#        print response.context
-#        self.failUnlessEqual(response.context['selection_note_all'], "2 Repositories")
+        # Check that the rendered context contains 1 repository.
+        #print response.context
+        #self.assertEqual(
+        #    response.context['selection_note_all'],
+        #    "1 Repository"
+        #)
