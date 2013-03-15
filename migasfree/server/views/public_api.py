620910899c81663cb936d4b4b1635dda594885c1
Jose Antonio Chavarría <jachavar@gmail.com>
2013-03-15 13:42:34 +0100

    Added get_computer_info view to know computer info by uuid

--- migasfree/server/views/public_api.py
+++ migasfree/server/views/public_api.py
@@ -4,9 +4,13 @@ import json
 
 from django.http import HttpResponse
 
-from migasfree.server.models import Platform
-from migasfree.server.models import Version
-
+from migasfree.server.models import (
+    Platform,
+    Version,
+    Computer,
+    Property,
+    Attribute
+)
 
 def get_versions(request):
     result=[]
@@ -20,5 +24,30 @@ def get_versions(request):
             element["versions"].append({"name": _version.name})
 
         result.append(element)
+
+    return HttpResponse(json.dumps(result), mimetype="text/plain")
+
+
+def get_computer_info(request):
+    result = {}
+    uuid = request.GET.get('uuid', '')
+
+    if Computer.objects.filter(uuid=uuid):
+        computer = Computer.objects.get(uuid=uuid)
+        result["id"] = computer.id
+        result["uuid"] = computer.uuid
+        result["name"] = computer.name
+
+        element = []
+        for tag in computer.tags.all():
+            element.append("%s-%s" % (tag.property_att.prefix, tag.value))
+        result["tags"] = element
+        result["available_tags"] = {}
+
+        for prp in Property.objects.filter(tag=True):
+            result["available_tags"][prp.name] = []
+            for tag in Attribute.objects.filter(property_att=prp):
+                result["available_tags"][prp.name].append("%s-%s" %
+                    (prp.prefix, tag.value))
 
     return HttpResponse(json.dumps(result), mimetype="text/plain")
