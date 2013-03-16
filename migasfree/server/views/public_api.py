516155a6d92f91bfc959128ce301ad8f2dd8760e
Jose Antonio Chavarría <jachavar@gmail.com>
2013-03-16 18:48:44 +0100

    Refactorized code with get_object_or_404 function

--- migasfree/server/views/public_api.py
+++ migasfree/server/views/public_api.py
@@ -3,7 +3,8 @@
 import json
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from migasfree.settings import (
     MIGASFREE_HELP_DESK,
     MIGASFREE_COMPUTER_SEARCH_FIELDS
@@ -35,28 +36,29 @@ def get_versions(request):
 
 
 def get_computer_info(request):
-    result = {}
     uuid = request.GET.get('uuid', '')
 
-    if Computer.objects.filter(uuid=uuid):
-        computer = Computer.objects.get(uuid=uuid)
-        result["id"] = computer.id
-        result["uuid"] = computer.uuid
-        result["name"] = computer.name
-        result["helpdesk"] = MIGASFREE_HELP_DESK
-        result["search"] = result[MIGASFREE_COMPUTER_SEARCH_FIELDS[0]]
+    computer = get_object_or_404(Computer, uuid=uuid)
 
-        element = []
-        for tag in computer.tags.all():
-            element.append("%s-%s" % (tag.property_att.prefix, tag.value))
-        result["tags"] = element
-        result["available_tags"] = {}
+    result = {
+        'id': computer.id,
+        'uuid': computer.uuid,
+        'name': computer.name,
+        'helpdesk': MIGASFREE_HELP_DESK,
+    }
+    result["search"] = result[MIGASFREE_COMPUTER_SEARCH_FIELDS[0]]
 
-        for prp in Property.objects.filter(tag=True):
-            result["available_tags"][prp.name] = []
-            for tag in Attribute.objects.filter(property_att=prp):
-                result["available_tags"][prp.name].append("%s-%s" %
-                    (prp.prefix, tag.value))
+    element = []
+    for tag in computer.tags.all():
+        element.append("%s-%s" % (tag.property_att.prefix, tag.value))
+    result["tags"] = element
+
+    result["available_tags"] = {}
+    for prp in Property.objects.filter(tag=True):
+        result["available_tags"][prp.name] = []
+        for tag in Attribute.objects.filter(property_att=prp):
+            result["available_tags"][prp.name].append("%s-%s" %
+                (prp.prefix, tag.value))
 
     return HttpResponse(json.dumps(result), mimetype="text/plain")
 
@@ -65,20 +67,20 @@ def computer_label(request):
     """
     To Print a Computer Label
     """
-    result = {}
     uuid = request.GET.get('uuid', '')
 
-    if Computer.objects.filter(uuid=uuid):
-        computer = Computer.objects.get(uuid=uuid)
-        result["id"] = computer.id
-        result["uuid"] = computer.uuid
-        result["name"] = computer.name
-        result["helpdesk"] = MIGASFREE_HELP_DESK
-        result["search"] = result[MIGASFREE_COMPUTER_SEARCH_FIELDS[0]]
-        return render(
-            request,
-            'server/computer_label.html',
-            result
-        )
-    else:
-        return HttpResponse("", mimetype="text/plain")
+    computer = get_object_or_404(Computer, uuid=uuid)
+
+    result = {
+        'id': computer.id,
+        'uuid': computer.uuid,
+        'name': computer.name,
+        'helpdesk': MIGASFREE_HELP_DESK,
+    }
+    result["search"] = result[MIGASFREE_COMPUTER_SEARCH_FIELDS[0]]
+
+    return render(
+        request,
+        'server/computer_label.html',
+        result
+    )
