b15d4fb569af8d5da16d19e4f77320dd3a149d39
Jose Antonio Chavarría <jachavar@gmail.com>
2014-01-29 19:57:47 +0100

    Erased unused import (get_current_status) (issue #47)

--- migasfree/server/context_processors.py
+++ migasfree/server/context_processors.py
@@ -2,7 +2,6 @@
 
 from migasfree.server.models import get_version_names, get_query_names, \
     UserProfile
-from migasfree.server.views import get_current_status
 
 
 def query_names(request):
@@ -19,8 +18,3 @@ def version_names(request):
         'version_names': get_version_names(),
         'current_version': current
     }
-
-
-def current_status(request):
-    #return {'current_status': get_current_status()}
-    return {'current_status': ''}
