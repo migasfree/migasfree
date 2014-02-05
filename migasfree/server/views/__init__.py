b6d5208482e965706e95ace8efd5601e7e2d0549
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-05 19:57:17 +0100

    Erased chart view function (not needed) (issue #4)

--- migasfree/server/views/__init__.py
+++ migasfree/server/views/__init__.py
@@ -2,9 +2,8 @@
 
 # import order is very important!!!
 
-from migasfree.server.views.stats import chart, \
-    daily_updated, delay_schedule, hourly_updated, monthly_updated, \
-    version_computer
+from migasfree.server.views.stats import daily_updated, delay_schedule, \
+    hourly_updated, monthly_updated, version_computer
 from migasfree.server.views.queries import query, \
     computer_messages, server_messages
 from migasfree.server.views.hardware import hardware, hardware_resume, \
@@ -12,10 +11,10 @@ from migasfree.server.views.hardware import hardware, hardware_resume, \
 #from migasfree.server.views.devices import device, load_devices
 from migasfree.server.views.repository import create_physical_repository
 from migasfree.server.views.client_api import api
-from migasfree.server.views.public_api import get_versions, get_computer_info, computer_label
+from migasfree.server.views.public_api import get_versions, get_computer_info, \
+    computer_label
 from migasfree.server.views.packages import change_version, info
 from migasfree.server.views.dashboard import alerts
 from migasfree.server.views.admin import connections_model
 
 from migasfree.server.views.login import login
-
