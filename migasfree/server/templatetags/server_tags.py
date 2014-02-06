ba5f548a9a50c205298504b8372611f1445dc609
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-06 21:55:27 +0100

    Adapt code to Django 1.6 (issue #49)

--- migasfree/server/templatetags/server_tags.py
+++ migasfree/server/templatetags/server_tags.py
@@ -8,9 +8,19 @@ from migasfree.settings import MIGASFREE_ORGANIZATION
 
 register = template.Library()
 
+
 class TemplateOrganization(template.Node):
     def render(self, context):
         return  MIGASFREE_ORGANIZATION
+
+
+@register.tag
+def organization(parser, token):
+    """
+    Return the variable 'ORGANIZATION'.
+    """
+    return TemplateOrganization()
+
 
 class TemplateVersion(template.Node):
     def render(self, context):
@@ -22,15 +32,6 @@ class TemplateVersion(template.Node):
 
         return obj
 
-@register.tag
-def organization(parser, token):
-    """
-    Return the variable 'ORGANIZATION'.
-    """
-    return TemplateOrganization()
-
-organization = register.tag(organization)
-
 
 @register.tag
 def version(parser, token):
@@ -39,7 +40,6 @@ def version(parser, token):
     """
     return TemplateVersion()
 
-version = register.tag(version)
 
 class TemplateLink(template.Node):
     def render(self, context):
@@ -53,6 +53,7 @@ class TemplateLink(template.Node):
 
         return obj
 
+
 @register.tag
 def link(parser, token):
     """
@@ -60,36 +61,33 @@ def link(parser, token):
     """
     return TemplateLink()
 
-link = register.tag(link)
 
-
-# Modified for add permissions "can_save"
+@register.inclusion_tag('admin/submit_line.html', takes_context=True)
 def submit_row(context):
     """
     Displays the row of buttons for delete and save.
     We have modified standard submit_row tag for add permissions 'can_save'
+
+    http://stackoverflow.com/questions/9179505/how-to-add-button-to-submit-row-context-in-django
     """
     opts = context['opts']
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
     user_id = context['user'].id
-    can_save = UserSystem.objects.get(id=user_id).has_perm('server.can_save_%s' % opts.module_name)
+    can_save = UserSystem.objects.get(id=user_id).has_perm(
+        'server.can_save_%s' % opts.module_name
+    )
 
     return {
-        'onclick_attrib': (opts.get_ordered_objects() and change
-                            and 'onclick="submitOrderForm();"' or ''),
         'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+            and (change or context['show_delete'])),
         'show_save_as_new': not is_popup and change and save_as and can_save,
         'show_save_and_add_another': context['has_add_permission'] and
-                            not is_popup and (not save_as or context['add']) and can_save,
-        'show_save_and_continue': not is_popup and context['has_change_permission'] and can_save,
+            not is_popup and (not save_as or context['add'])
+            and can_save,
+        'show_save_and_continue': not is_popup
+            and context['has_change_permission'] and can_save,
         'is_popup': is_popup,
         'show_save': context['add'] or (can_save),
     }
-
-submit_row = register.inclusion_tag(
-    'admin/submit_line.html',
-    takes_context=True
-)(submit_row)
