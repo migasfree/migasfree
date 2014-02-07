c192ec272bb4da759ff59b0d4b0ff54296fa0062
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-07 13:43:47 +0100

    Fixed variable PATH (issue #32)

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ if not hasattr(sys, 'version_info') or sys.version_info < (2, 6, 0, 'final'):
     raise SystemExit('migasfree-client requires Python 2.6 or later.')
 
 import os
-README = open(os.path.join(os.path.dirname(__file__), 'README')).read()
+PATH = os.path.dirname(__file__)
+README = open(os.path.join(PATH, 'README')).read()
 VERSION = open(os.path.join(PATH, 'VERSION')).read().splitlines()[0]
 
 #import glob
