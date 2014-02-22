d06006b692e9348fdac9a461d9302b8748ee5844
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-22 18:43:14 +0100

    Added metadata project

--- migasfree/__init__.py
+++ migasfree/__init__.py
@@ -1,3 +1,6 @@
-"""
-   migasfree server
-"""
+# -*- coding: UTF-8 -*-
+
+__version__ = "4.1"
+__author__ = "Alberto Gacías & Jose Antonio Chavarría"
+__contact__ = "fun.with@migasfree.org"
+__homepage__ = "https://github.com/migasfree/migasfree/"
