37e6c714cad94718aa86104cfd89b12cebf972ea
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-22 10:26:47 +0100

    Erased unused link method (issue #61)

--- migasfree/server/models/device_type.py
+++ migasfree/server/models/device_type.py
@@ -3,8 +3,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from common import link
-
 
 class DeviceType(models.Model):
     name = models.CharField(
@@ -23,9 +21,3 @@ class DeviceType(models.Model):
         verbose_name = _("Device (Type)")
         verbose_name_plural = _("Device (Types)")
         permissions = (("can_save_devicetype", "Can save Device Type"),)
-
-    def link(self):
-        return link(self, self._meta.object_name)
-
-    link.short_description = Meta.verbose_name
-    link.allow_tags = True
