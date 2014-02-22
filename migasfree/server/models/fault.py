3b0a729fb595704c4eaaedb718e2e8aa87d90b71
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-22 10:27:12 +0100

    Erased unused link method (issue #61)

--- migasfree/server/models/fault.py
+++ migasfree/server/models/fault.py
@@ -3,7 +3,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from migasfree.server.models.common import link
 from migasfree.server.models import Computer, FaultDef, Version
 
 
@@ -45,6 +44,9 @@ class Fault(models.Model):
     computer_link.allow_tags = True
     computer_link.short_description = _("Computer")
 
+    def list_users(self):
+        return self.faultdef.list_users()
+
     def __unicode__(self):
         return u'%s - %s - %s' % (
             str(self.id),
@@ -57,12 +59,3 @@ class Fault(models.Model):
         verbose_name = _("Fault")
         verbose_name_plural = _("Faults")
         permissions = (("can_save_fault", "Can save Fault"),)
-
-    def link(self):
-        return link(self, self._meta.object_name)
-
-    link.short_description = Meta.verbose_name
-    link.allow_tags = True
-
-    def list_users(self):
-        return self.faultdef.list_users()
