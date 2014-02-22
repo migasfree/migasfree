b736f318fdb5817b302d7bfe7c932658fb785f89
Jose Antonio Chavarría <jachavar@gmail.com>
2014-02-22 10:27:54 +0100

    Erased unused link method (issue #61)

--- migasfree/server/models/message_server.py
+++ migasfree/server/models/message_server.py
@@ -3,8 +3,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from common import link
-
 
 class MessageServer(models.Model):
     text = models.TextField(
@@ -26,9 +24,3 @@ class MessageServer(models.Model):
         verbose_name = _("Message Server")
         verbose_name_plural = _("Messages Server")
         permissions = (("can_save_messageserver", "Can save Message Server"),)
-
-    def link(self):
-        return link(self, self._meta.object_name)
-
-    link.short_description = Meta.verbose_name
-    link.allow_tags = True
