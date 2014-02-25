113bf2e62b163ee982a10cfbbdb663d0affcc6b8
tux <alberto@migasfree.org>
2014-02-25 09:00:23 +0100

    Included content of version_manager.py to can use user_version function (show related data only if current version) (issue #61)

--- migasfree/server/models/version.py
+++ migasfree/server/models/version.py
@@ -5,9 +5,15 @@ import shutil
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import (
+    User as UserSystem,
+    UserManager
+)
 from django.conf import settings
 
 from migasfree.server.models import Pms, Platform, MigasLink
+
+from migasfree.middleware import threadlocals
 
 
 class Version(models.Model, MigasLink):
@@ -65,7 +71,11 @@ class Version(models.Model, MigasLink):
         if not os.path.exists(_repos):
             os.makedirs(_repos)
 
-        _stores = os.path.join(settings.MIGASFREE_REPO_DIR, self.name, 'STORES')
+        _stores = os.path.join(
+            settings.MIGASFREE_REPO_DIR,
+            self.name,
+            'STORES'
+        )
         if not os.path.exists(_stores):
             os.makedirs(_stores)
 
@@ -73,14 +83,20 @@ class Version(models.Model, MigasLink):
         self.name = self.name.replace(" ", "-")
         self.create_dirs()
         self.base = self.base.replace("\r\n", "\n")
+
         super(Version, self).save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
-        # remove the directory of this version
         path = os.path.join(settings.MIGASFREE_REPO_DIR, self.name)
         if os.path.exists(path):
             shutil.rmtree(path)
+
         super(Version, self).delete(*args, **kwargs)
+
+    def link(self):
+        return super(Version, self).link(
+            default=(self.id != user_version().id)
+        )
 
     class Meta:
         app_label = 'server'
@@ -89,9 +105,71 @@ class Version(models.Model, MigasLink):
         permissions = (("can_save_version", "Can save Version"),)
 
 
+class VersionManager(models.Manager):
+    """
+    VersionManager is used for filter the property "objects" of several
+    classes by the version of user logged.
+    """
+    def get_query_set(self):
+        user = user_version()
+        if user is None:
+            return self.version(0)
+        else:
+            return self.version(user)
+
+    def version(self, version):
+        if version == 0:  # return the objects of ALL VERSIONS
+            return super(VersionManager, self).get_query_set()
+        else:  # return only the objects of this VERSION
+            return super(VersionManager, self).get_query_set().filter(
+                version=version
+            )
+
+
+class UserProfile(UserSystem):
+    """
+    info = 'For change password use <a href="%s">change password form</a>.' \
+        % reverse('admin:password_change')
+    """
+
+    version = models.ForeignKey(
+        Version,
+        verbose_name=_("version"),
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    # Use UserManager to get the create_user method, etc.
+    objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        if not (self.password.startswith("sha1$") or
+                self.password.startswith("pbkdf2")):
+            super(UserProfile, self).set_password(self.password)
+        super(UserProfile, self).save(*args, **kwargs)
+
+    class Meta:
+        app_label = 'server'
+        verbose_name = _("User Profile")
+        verbose_name_plural = _("User Profiles")
+        permissions = (("can_save_userprofile", "Can save User Profile"),)
+
+
 def get_version_names():
     result = []
     for item in Version.objects.all().order_by("name"):
         result.append([item.id, item.name])
 
     return result
+
+
+def user_version():
+    """
+    Return the user version that is logged
+    """
+    try:
+        return UserProfile.objects.get(
+            id=threadlocals.get_current_user().id
+        ).version
+    except:
+        return None
