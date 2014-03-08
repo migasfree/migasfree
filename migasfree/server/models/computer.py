3057059a31df76d2cb10f8ab7b4e859d36c041cf
Jose Antonio Chavarría <jachavar@gmail.com>
2014-03-08 09:57:40 +0100

    Erased unused import Q

--- migasfree/server/models/computer.py
+++ migasfree/server/models/computer.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 from django.template import Context, Template
 from django.conf import settings
