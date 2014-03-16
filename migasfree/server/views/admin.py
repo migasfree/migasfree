fc22339adf69a115f6e809cffe95e1b249ce001c
Jose Antonio Chavarría <jachavar@gmail.com>
2014-03-16 21:10:48 +0100

    Explicit relative imports
    Added login_required decorator
    
    If no id parameter, returns empty json

--- migasfree/server/views/admin.py
+++ migasfree/server/views/admin.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 *-*
+
+# FIXME why this file is called admin?
+
 from django.core import serializers
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from migasfree.server.models import  DeviceModel, DeviceConnection
-from migasfree.server.functions import *
 
+from .models import DeviceModel, DeviceConnection
+from .functions import s2l, vl2s
+
+
+@login_required
 def connections_model(request):
     json = "{}"
     model_id = request.GET.get('id', '')
-    lst_id = s2l(vl2s(DeviceModel.objects.get(id=model_id).connections))
-    json = serializers.serialize("json", DeviceConnection.objects.filter(id__in =lst_id))
+    if model_id != '':
+        lst_id = s2l(vl2s(DeviceModel.objects.get(id=model_id).connections))
+        json = serializers.serialize(
+            "json",
+            DeviceConnection.objects.filter(id__in=lst_id)
+        )
+
     return HttpResponse(json, mimetype="application/javascript")
