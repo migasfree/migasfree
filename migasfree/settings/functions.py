52ee68da1feb9c3648d709e83165b05058c459d2
Alberto Gacías <alberto@migasfree.org>
2014-08-28 19:21:15 +0200

    BugFix: if not exists path (close #100)

--- migasfree/settings/functions.py
+++ migasfree/settings/functions.py
@@ -2,12 +2,15 @@ import os
 import random
 import string
 
+
 def secret_key(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
     _file = os.path.join(path, 'secret_key.txt')
     if os.path.exists(_file):
         _key = open(_file).read().strip()
     else:
-        _key = ''.join([random.SystemRandom().choice("{}{}{}".format(string.ascii_letters, string.digits, string.punctuation)) for i in range(50)])
+        _key = ''.join([random.SystemRandom(). choice("{}{}{}". format(string.ascii_letters, string.digits, string.punctuation)) for i in range(50)])
         with file(_file, 'w') as outfile:
             outfile.write(_key)
     return _key
