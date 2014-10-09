120425c160a77b96375ce1a6a870151a404bf3e1
Jose Antonio Chavarría <jachavar@gmail.com>
2014-10-09 08:59:21 +0200

    Bumped version to 4.4

--- migasfree/__init__.py
+++ migasfree/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-__version__ = "4.3"
+__version__ = "4.4"
 __author__ = "Alberto Gacías & Jose Antonio Chavarría"
 __contact__ = "fun.with@migasfree.org"
 __homepage__ = "https://github.com/migasfree/migasfree/"
