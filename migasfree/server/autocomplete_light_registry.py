d50c83f8c925385e8823de85712e1cc5bbddce7d
Jose Antonio Chavarría <jachavar@gmail.com>
2015-11-13 13:37:06 +0100

    Added new app to improve UX in normal forms (issue #137)

---
+++ migasfree/server/autocomplete_light_registry.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import autocomplete_light.shortcuts as al
+
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Computer
+
+
+class ComputerAutocomplete(al.AutocompleteModelBase):
+    search_fields = (settings.MIGASFREE_COMPUTER_SEARCH_FIELDS)
+    model = Computer
+    attrs = {
+        'placeholder': _('Computer'),
+        'data-autocomplete-minimum-characters': 3,
+    }
+    widget_attrs={
+        'data-widget-maximum-values': 1,
+    }
+
+al.register(ComputerAutocomplete)
