374f5b305f9a9b66a6cc558642fc2ce175c1ab10
Jose Antonio Chavarría <jachavar@gmail.com>
2015-12-06 18:10:31 +0100

    Added item_at_index, get_sets and process static methods

--- migasfree/server/models/attribute_set.py
+++ migasfree/server/models/attribute_set.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from migasfree.server.models import (
+from . import (
     Property,
     Attribute,
     MigasLink
@@ -47,6 +47,76 @@ class AttributeSet(models.Model, MigasLink):
 
     def __unicode__(self):
         return self.name
+
+    @staticmethod
+    def item_at_index(lst, item, before=-1):
+        try:
+            id_before = lst.index(before)
+        except:
+            id_before = -1
+
+        try:
+            id_item = lst.index(item)
+        except:
+            id_item = -1
+
+        if id_item == -1:
+            if id_before == -1:
+                lst.append(item)
+            else:
+                lst.insert(id_before, item)
+        else:
+            if id_before > -1:
+                if id_before < id_item:
+                    lst = lst[0:id_before] + lst[id_item:] \
+                        + lst[id_before:id_item]
+
+        return lst
+
+    @staticmethod
+    def get_sets():
+        sets = []
+        for item in AttributeSet.objects.filter(active=True):
+            sets = AttributeSet.item_at_index(sets, item.id)
+
+            for subset in item.included_attributes.filter(
+                id__gt=1
+            ).filter(
+                property_att__id=1
+            ).filter(~Q(value=item.name)):
+                sets = AttributeSet.item_at_index(
+                    sets,
+                    AttributeSet.objects.get(name=subset.value).id,
+                    before=item.id
+                )
+
+            for subset in item.excludes.filter(
+                id__gt=1
+            ).filter(
+                property_att__id=1
+            ).filter(~Q(value=item.name)):
+                sets = AttributeSet.item_at_index(
+                    sets,
+                    AttributeSet.objects.get(name=subset.value).id,
+                    before=item.id
+                )
+
+        return sets
+
+    @staticmethod
+    def process(attributes):
+        property_set = Property.objects.get(id=1)
+
+        att_id = []
+        for item in AttributeSet.get_sets():
+            for soa in AttributeSet.objects.filter(id=item).filter(
+                Q(attributes__id__in=attributes)
+            ).filter(~Q(excludes__id__in=attributes)):
+                att_id.append(
+                    Attribute.objects.create(property_set, soa.name).id
+                )
+
+        return att_id
 
     class Meta:
         app_label = 'server'
