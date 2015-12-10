92fa6fd99826820959aac2d6be364b058ed3640e
Jose Antonio Chavarría <jachavar@gmail.com>
2015-12-10 11:48:30 +0100

    Replaced ugettext by ugettext_lazy (Django warning) (issue #118)

--- migasfree/server/errmfs.py
+++ migasfree/server/errmfs.py
@@ -7,7 +7,7 @@ import tempfile
 
 from django.template.loader import render_to_string
 from django.conf import settings
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 ALL_OK = 0
