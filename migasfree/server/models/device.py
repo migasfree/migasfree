5262ca97f25f0573234c93126f7630cd7c977b38
Jose Antonio Chavarría <jachavar@gmail.com>
2016-03-08 09:24:16 +0100

    Added pre_save signal to bugfix #159 (closes #159)
    Replaced spaces in alternative NAME

--- migasfree/server/models/device.py
+++ migasfree/server/models/device.py
@@ -3,6 +3,8 @@
 import json
 
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
@@ -38,10 +40,7 @@ class Device(models.Model, MigasLink):
 
     def location(self):
         data = json.loads(self.data)
-        if 'LOCATION' in data:
-            return data['LOCATION']
-        else:
-            return ""
+        return data.get('LOCATION', '')
 
     def model_link(self):
         return self.model.link()
@@ -59,9 +58,28 @@ class Device(models.Model, MigasLink):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        data = json.loads(self.data)
+        if 'NAME' in data:
+            data['NAME'] = data['NAME'].replace(' ', '_')
+            self.data = json.dumps(data)
+
+        super(Device, self).save(*args, **kwargs)
+
     class Meta:
         app_label = 'server'
         verbose_name = _("Device")
         verbose_name_plural = _("Devices")
         unique_together = (("connection", "name"),)
         permissions = (("can_save_device", "Can save Device"),)
+
+
+@receiver(pre_save, sender=Device)
+def pre_save_device(sender, instance, **kwargs):
+    if instance.id:
+        old_obj = Device.objects.get(pk=instance.id)
+        if old_obj.data != instance.data:
+            for logical_device in instance.devicelogical_set.all():
+                print logical_device.id
+                for computer in logical_device.computer_set.all():
+                    computer.remove_device_copy(logical_device.id)
