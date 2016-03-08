9a6d13b117351595b4fb83659de55660cbf3d126
Jose Antonio Chavarría <jachavar@gmail.com>
2016-03-08 21:44:32 +0100

    Django autocomplete light upgraded to version 3 (issue #157)

--- migasfree/urls.py
+++ migasfree/urls.py
@@ -15,7 +15,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^autocomplete/', include('autocomplete_light.urls')),
     url(r'^admin/lookups/', include(ajax_select_urls)),
 )
 
