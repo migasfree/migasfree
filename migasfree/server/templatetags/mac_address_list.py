edd237722038232ff311eda3b23d927ff8313f18
agacias <alberto@migasfree.org>
2016-03-10 09:06:01 +0100

    Added mac_address_list template tag (used in admin/includes/fieldset.html for certain read only fields) (issue #161)

---
+++ migasfree/server/templatetags/mac_address_list.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+import re
+
+register = template.Library()
+
+MAC_RAW_LEN = 12
+
+
+@register.filter(name='mac_address_list')
+def mac_address_list(value):
+    """
+    {{ mac_address|mac_address_list }}
+    http://stackoverflow.com/questions/8346735/
+    inserting-a-character-at-regular-intervals-in-a-list
+    """
+    ret = []
+    for i in range(0, len(value), MAC_RAW_LEN):
+        ret.append(':'.join(re.findall('..', value[i:i + MAC_RAW_LEN])))
+
+    return ', '.join(ret)
