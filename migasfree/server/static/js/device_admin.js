2b61bfea616f8281d4ba4d56d77c7a074c634850
Jose Antonio Chavarría <jachavar@gmail.com>
2016-06-01 09:06:40 +0200

    JavaScript code styles!!!

--- migasfree/server/static/js/device_admin.js
+++ migasfree/server/static/js/device_admin.js
@@ -1,73 +1,73 @@
-function change_data()
+function changeData()
 {
     if ($("#id_model").val() !== "")
     {
-        $.getJSON("/connections_model/?id=" + $("#id_model").val(), function(connection_list)
+        $.getJSON("/connections_model/?id=" + $("#id_model").val(), function(connectionList)
         {
-            var html = '';
-            for (var i = 0; i < connection_list.length; i++)
+            var html = "";
+            for (var i = 0; i < connectionList.length; i++)
             {
-                if (connection_list[i].pk === $("#id_connection").val())
+                if (connectionList[i].pk === parseInt($("#id_connection").val()))
                 {
-                    if (connection_list[i].fields.fields !== '')
+                    if (connectionList[i].fields.fields !== "")
                     {
-                        html += '<div class="row join">';
-                        var fields = connection_list[i].fields.fields.split(',');
+                        html += "<div class='row join'>";
+                        var fields = connectionList[i].fields.fields.split(",");
                         var data = eval("(" + $("#id_data").val() + ")");
                         for (var j = 0; j < fields.length; j++)
                         {
-                            var value = (typeof data[fields[j]] !== 'undefined') ? data[fields[j]] : '';
-                            html += '<div class="control-group">';
-                            html += '<div class="col-md-12 form-group ">';
-                            html += '<div class="control-label col-sm-3">';
-                            html += '<label for="join_' + fields[j] + '">' + fields[j] + '</label>';
-                            html += '</div>';
-                            html += '<div class="controls col-sm-9">';
-                            html += '<input id="join_' + fields[j] + '" type="text" class="form-control join_field" value="' + value + '" />';
-                            html += '</div>';
-                            html += '</div>';
-                            html += '</div>';
+                            var value = (typeof data[fields[j]] !== "undefined") ? data[fields[j]] : "";
+                            html += "<div class='control-group'>";
+                            html += "<div class='col-md-12 form-group'>";
+                            html += "<div class='control-label col-sm-3'>";
+                            html += "<label for='join_" + fields[j] + "'>" + fields[j] + "</label>";
+                            html += "</div>";
+                            html += "<div class='controls col-sm-9'>";
+                            html += "<input id='join_" + fields[j] + "' type='text' class='form-control join_field' value='" + value + "' />";
+                            html += "</div>";
+                            html += "</div>";
+                            html += "</div>";
                         }
-                        html += '</div>';
+                        html += "</div>";
                     }
                 }
             }
 
             // remove fields
-            $('div.join').remove();
+            $("div.join").remove();
 
             // append new related fields
-            $('div.fields').append(html);
+            $("div.fields").append(html);
         });
     }
 }
 
 
-function change_model(default_value)
+function changeModel(defaultValue)
 {
-    default_value = default_value || '';
+    defaultValue = defaultValue || "";
 
-    if ($("#id_model").val() === '')
+    if ($("#id_model").val() === "")
     {
-        $("#id_connection").attr('disabled', true);
+        $("#id_connection").attr("disabled", true);
     }
     else
     {
         $.getJSON("/connections_model/?id="+$("#id_model").val(), function(j) {
-            var options = '';
+            var options = "";
             for (var i = 0; i < j.length; i++) {
-                options += '<option value="' + parseInt(j[i].pk) + '">' + j[i].fields.name + '</option>';
+                options += "<option value='" + parseInt(j[i].pk) + "'>" + j[i].fields.name + "</option>";
             }
             $("#id_connection").html(options);
-            $("#id_connection").attr('disabled', false);
+            $("#id_connection").attr("disabled", false);
 
-            if (typeof default_value === 'string' && default_value !== '')
+            if (typeof defaultValue === "string" && defaultValue !== "")
             {
-                $('#id_connection option[value=' + default_value + ']').attr('selected', true);
+                $("#id_connection option[value=" + defaultValue + "]").attr("selected", true);
             }
         });
-        $("#id_model").attr('selected', 'selected');
-        change_data();
+        $("#id_model").attr("selected", "selected");
+        changeData();
     }
 }
 
@@ -78,24 +78,24 @@ $(function() {
         $("#id_data").closest("div").hide();
         $("#id_data").attr("hidden", "hidden");
 
-        if ($("#id_model").val() === '')
+        if ($("#id_model").val() === "")
         {
-            $("#id_connection").attr('disabled', true);
+            $("#id_connection").attr("disabled", true);
         }
 
-        change_model($("#id_connection").val());
+        changeModel($("#id_connection").val());
 
-        $("#id_model").change(change_model);
-        $("#id_connection").change(change_data);
+        $("#id_model").change(changeModel);
+        $("#id_connection").change(changeData);
 
         $('#device_form').submit(function(event) {
-            var data = '';
-            $('.join_field').each(function(i) {
-                data += '"' + $(this).attr('id').substring('join_'.length) + '"';
-                data += ':';
-                data += '"' + $(this).val() + '",';
+            var data = "";
+            $(".join_field").each(function(i) {
+                data += "'" + $(this).attr("id").substring("join_".length) + "'";
+                data += ":";
+                data += "'" + $(this).val() + "',";
             });
-            $('#id_data').val('{' + data.substring(0, data.length - 1) + '}');
+            $('#id_data').val("{" + data.substring(0, data.length - 1) + "}");
 
             return;
         });
