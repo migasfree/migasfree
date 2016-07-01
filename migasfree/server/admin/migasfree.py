736282661f72ada17957a38cd4c29ee6458546d9
Jose Antonio Chavarría <jachavar@gmail.com>
2016-07-01 12:56:57 +0200

    boolean_field converted in static method

--- migasfree/server/admin/migasfree.py
+++ migasfree/server/admin/migasfree.py
@@ -8,7 +8,8 @@ from import_export.admin import ExportActionModelAdmin
 class MigasAdmin(ExportActionModelAdmin):
     list_display_links = None
 
-    def boolean_field(self, field):
+    @staticmethod
+    def boolean_field(field):
         if field:
             ret = '<span class="fa fa-check boolean-yes">' \
                 + '<span class="sr-only">%s</span></span>' % ugettext('Yes')
