b79ac9bfd729a04beb9be739d8dfab871d915e2e
Jose Antonio Chavarría <jachavar@gmail.com>
2016-07-21 11:54:33 +0200

    Refactored file functions

--- migasfree/server/views/dashboard.py
+++ migasfree/server/views/dashboard.py
@@ -16,23 +16,24 @@ def execute_active_checkings(request):
     Returns active checkings results, as a list
     If an error occurs, returns a dictionary with execution error information
     If no results, returns empty list
+
+    request parameter maybe used in exec code call (not unused!!!)
     """
-    alerts = []
+    checkings = []
     for check in Checking.objects.filter(active=True).order_by('alert'):
         try:
             exec(check.code.replace("\r", ""))
         except:
             return {
                 'error': {
-                    'description': _("Error in field 'code' of Checking: %s")
-                        % check.name,
-                    'contentpage': '%s\n%s' % (check.code, str(sys.exc_info()))
+                    'description': _("Error in field 'code' of Checking: %s") % check.name,
+                    'contentpage': '{}\n{}'.format(check.code, sys.exc_info())
                 }
             }
 
         result = vars().get('result', 0)
         if result > 0:
-            alerts.append({
+            checkings.append({
                 'badge': result,
                 'alert': vars().get('alert', 'info'),
                 'target': vars().get('target', 'computer'),
@@ -42,7 +43,7 @@ def execute_active_checkings(request):
 
         vars().clear()
 
-    return alerts
+    return checkings
 
 
 @permission_required('server.change_checking', raise_exception=True)
@@ -51,25 +52,23 @@ def alerts(request):
     """
     Status of checkings
     """
-    template = 'includes/alerts.html'
-
-    alerts = execute_active_checkings(request)
-    if isinstance(alerts, dict) and alerts.get('error').get('description'):
+    checkings = execute_active_checkings(request)
+    if isinstance(checkings, dict) and checkings.get('error').get('description'):
         return render(
             request,
             'error.html',
             {
-                'description': alerts.get('error').get('description'),
-                'contentpage': alerts.get('error').get('contentpage')
+                'description': checkings.get('error').get('description'),
+                'contentpage': checkings.get('error').get('contentpage')
             }
         )
 
     return render(
         request,
-        template,
+        'includes/alerts.html',
         {
             'title': _('Status'),
-            'alerts': alerts,
-            'badge': sum(row['badge'] for row in alerts),
+            'alerts': checkings,
+            'badge': sum(row['badge'] for row in checkings),
         }
     )
