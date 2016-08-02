0dfe58b6a9a395c7616f2991189d597b5536c5a4
Jose Antonio Chavarría <jachavar@gmail.com>
2016-08-02 12:53:18 +0200

    Added catalog and markdownx routes (issue #177)
    Changed token-auth routes (preparing code to Django 1.10)

--- migasfree/urls.py
+++ migasfree/urls.py
@@ -5,16 +5,19 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 
 from ajax_select import urls as ajax_select_urls
+from rest_framework.authtoken import views
 
 from server.routers import router
+from catalog.routers import router as catalog_router
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^token-auth/$', 'rest_framework.authtoken.views.obtain_auth_token'),
+    url(r'^token-auth/$', views.obtain_auth_token),
     url(r'^api/v1/token/', include(router.urls)),
+    url(r'^api/v1/public/catalog/', include(catalog_router.urls)),
 
     url(r'', include('migasfree.server.urls')),
 
@@ -22,6 +25,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^admin/lookups/', include(ajax_select_urls)),
+    url(r'^markdownx/', include('markdownx.urls')),
 ]
 
 if settings.DEBUG and settings.STATIC_ROOT is not None:
