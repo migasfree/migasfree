ede6a38a8f8b3da0df509905fd0847ebd8cf3810
Jose Antonio Chavarría <jachavar@gmail.com>
2016-09-07 15:13:02 +0200

    Added REST_FRAMEWORK['DATETIME_FORMAT'] setting to fix a Chrome/Chromium bug with dates without timezone information

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -134,6 +134,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DATETIME_FORMAT': '%c',
 }
 
 DAB_FIELD_RENDERER = 'django_admin_bootstrapped.renderers.BootstrapFieldRenderer'
