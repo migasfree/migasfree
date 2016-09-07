5529edd40e1f590acfd7e557722253bf1ce55403
Jose Antonio Chavarría <jachavar@gmail.com>
2016-09-07 20:18:07 +0200

    PEP8 cleanup

--- migasfree/settings/functions.py
+++ migasfree/settings/functions.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import random
 import string
@@ -6,11 +8,17 @@ import string
 def secret_key(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
     _file = os.path.join(path, 'secret_key.txt')
     if os.path.exists(_file):
         _key = open(_file).read().strip()
     else:
-        _key = ''.join([random.SystemRandom().choice("%s%s%s" % (string.ascii_letters, string.digits, string.punctuation)) for i in range(50)])
+        _key = ''.join(
+            [random.SystemRandom().choice(
+                "%s%s%s" % (string.ascii_letters, string.digits, string.punctuation)
+            ) for i in range(50)]
+        )
         with file(_file, 'w') as outfile:
             outfile.write(_key)
+
     return _key
