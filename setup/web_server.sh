732f30df9e6d072d1a2fc486546f489ff665589d
Jose Antonio Chavarría <jachavar@gmail.com>
2016-10-19 09:21:00 +0200

    Changed configuration directories order detection in get_config_path function (closes #188)

--- setup/web_server.sh
+++ setup/web_server.sh
@@ -43,7 +43,7 @@ function get_apache_version()
     _NAME=$(get_apache_name)
     if [ -n "$_NAME" ]
     then
-        echo -n $($_NAME -v | grep '^Server version' | cut -d ' ' -f 3 | cut -d '/' -f 2)
+        echo -n $("$_NAME" -v | grep '^Server version' | cut -d ' ' -f 3 | cut -d '/' -f 2)
     else
         echo ""
     fi
@@ -51,18 +51,18 @@ function get_apache_version()
 
 function get_config_path()
 {
-    if [ -d "/etc/apache/conf.d" ]
+    if [ -d "/etc/apache2/sites-enabled" ]
     then
-        echo "/etc/apache/conf.d"
+        echo "/etc/apache2/sites-enabled"
     elif [ -d "/etc/apache2/conf.d" ]
     then
         echo "/etc/apache2/conf.d"
+    elif [ -d "/etc/apache/conf.d" ]
+    then
+        echo "/etc/apache/conf.d"
     elif [ -d "/etc/httpd/conf.d" ]
     then
         echo "/etc/httpd/conf.d"
-    elif [ -d "/etc/apache2/sites-enabled" ]
-    then
-        echo "/etc/apache2/sites-enabled"
     else
         echo ""
     fi
@@ -91,7 +91,7 @@ function create_apache_config()
     _MIGASFREE_REPO_DIR=$(get_migasfree_setting MIGASFREE_REPO_DIR)
     _MIGASFREE_APP_DIR=$(get_migasfree_setting MIGASFREE_APP_DIR)
 
-    _CONF_FILE=$_CONF_PATH/migasfree.conf
+    _CONF_FILE="$_CONF_PATH"/migasfree.conf
     cat > $_CONF_FILE << EOF
 Alias /static $_STATIC_ROOT
 <Directory $_STATIC_ROOT>
@@ -129,17 +129,17 @@ function set_web_server_permissions()
     _USER=$(get_user_web_service)
     # owner for repositories
     _REPO_PATH=$(get_migasfree_setting MIGASFREE_REPO_DIR)
-    owner $_REPO_PATH $_USER
+    owner "$_REPO_PATH" "$_USER"
 
     # owner for keys
     _KEYS_PATH=$(get_migasfree_setting MIGASFREE_KEYS_DIR)
-    owner $_KEYS_PATH $_USER
-    chmod 700 $_KEYS_PATH
+    owner "$_KEYS_PATH" "$_USER"
+    chmod 700 "$_KEYS_PATH"
 
     # owner for migasfree.log
     _TMP_DIR=$(get_migasfree_setting MIGASFREE_TMP_DIR)
     touch "$_TMP_DIR/migasfree.log"
-    owner "$_TMP_DIR/migasfree.log" $_USER
+    owner "$_TMP_DIR/migasfree.log" "$_USER"
 }
 
 function web_server_init()
