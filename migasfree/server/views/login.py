6758f278342871c485dfe496ed31b0200bb0ea4a
Jose Antonio Chavarría <jachavar@gmail.com>
2016-10-27 18:59:27 +0200

    Removed change_version (replaced by preferences view) (issue #187)

--- migasfree/server/views/login.py
+++ migasfree/server/views/login.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.utils.translation import ugettext as _
-from django.contrib import auth
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+from ..models import Version, UserProfile
 
 
 def login(request):
@@ -32,3 +35,25 @@ def login(request):
     return HttpResponseRedirect(
         request.GET.get('next', reverse('bootstrap'))
     )
+
+
+@login_required
+def preferences(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            'preferences.html',
+        )
+
+    version = get_object_or_404(Version, pk=request.POST.get('version', 0))
+    user_profile = UserProfile.objects.get(id=request.user.id)
+    user_profile.update_version(version)
+
+    messages.success(request, _("Preferences changed"))
+
+    success_url = '%s?active__exact=1&version__id__exact=%d' % (
+        reverse('admin:server_repository_changelist'),
+        version.id
+    )
+
+    return redirect(success_url)
