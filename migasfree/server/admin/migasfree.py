15c78c0c9856809874d35dcda25d987e20a0a554
Alberto Gacias <alberto@migasfree.org>
2016-10-30 09:31:43 +0100

    Show filters selected in change list (#187)

--- migasfree/server/admin/migasfree.py
+++ migasfree/server/admin/migasfree.py
@@ -3,10 +3,14 @@
 from django.utils.translation import ugettext
 
 from import_export.admin import ExportActionModelAdmin
+from django.contrib.admin.views.main import ChangeList
+from django.utils.translation import ugettext_lazy as _
+from django.db.models.fields import BooleanField
 
 
 class MigasAdmin(ExportActionModelAdmin):
     list_display_links = None
+    filter_description = ""
 
     @staticmethod
     def boolean_field(field):
@@ -19,3 +23,57 @@ class MigasAdmin(ExportActionModelAdmin):
         return '<span class="fa %s"><span class="sr-only">%s</span></span>' % (
             style, text
         )
+
+    def get_changelist(self, request, **kwargs):
+        return MigasChangeList
+
+
+class MigasChangeList(ChangeList):
+    def __init__(self, *args, **kwargs):
+        super(MigasChangeList, self).__init__(*args, **kwargs)
+        filters = []
+        for x in self.filter_specs:
+            if hasattr(x, 'lookup_choices') and hasattr(x, 'used_parameters') and x.used_parameters:
+
+                if isinstance(x.lookup_choices[0][0], int):
+                    element = dict(x.lookup_choices)[int(x.used_parameters.values()[0])]
+                else:
+                    element = dict(x.lookup_choices)[x.used_parameters.values()[0]]
+
+                filters.append(u"%s:%s" % (x.title, element))
+
+            elif (hasattr(x, 'lookup_choices')
+                and hasattr(x, "lookup_val")
+                and x.lookup_val):
+
+                if isinstance(x.lookup_choices[0][0], int):
+                    filters.append( u"%s:%s" % ( x.lookup_title, dict(x.lookup_choices)[int(x.lookup_val)]))
+                else:
+                    filters.append( u"%s:%s" % ( x.lookup_title, dict(x.lookup_choices)[x.lookup_val] ))
+
+            elif hasattr(x, 'links'):
+                for l in x.links:
+                    if l[1] == x.used_parameters:
+                        filters.append(u"%s:%s" % (x.title, unicode(l[0])))
+                        break
+
+            elif hasattr(x,'field') and hasattr(x.field,'choices') and hasattr(x,'lookup_val') and x.lookup_val:
+
+                if isinstance(x.field, BooleanField):
+                    if x.lookup_val == "0":
+                        filters.append(u"%s:%s" % (x.title, _("No")))
+                    else:
+                        filters.append(u"%s:%s" % (x.title, _("Yes")))
+                else:
+                    elements = []
+                    for element in x.lookup_val.split(','):
+                        elements.append(unicode(dict(x.field.choices)[element]))
+                    filters.append(u"%s:%s" % (x.title, "-".join(elements)))
+
+        filter_description = u', '.join(filters)
+        self.title = u"%s %s" % (
+           self.model._meta.verbose_name_plural,
+           filter_description
+        )
+
+        self.filter_description = filter_description
