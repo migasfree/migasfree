24f769171061ff07c6bfe60d648ccce64c395319
Jose Antonio Chavarría <jachavar@gmail.com>
2016-11-17 16:37:49 +0100

    JavaScript guidelines...

--- migasfree/server/static/js/package_admin.js
+++ migasfree/server/static/js/package_admin.js
@@ -1,25 +1,28 @@
-function getoptions(endpoint,key,value)
+function getOptions(url, key, value)
 {
     var _options = "";
     $.ajaxSetup({async: false});
-    while (endpoint) {
-        $.getJSON(endpoint, function(q) {
-            var j=q["results"];
+    while(url) {
+        $.getJSON(url, function(q) {
+            var j = q["results"];
             for (var i = 0; i < j.length; i++) {
                 _options += "<option value='" + j[i][key] + "'>" + j[i][value] + "</option>";
             }
-            endpoint=q["next"]
+            url = q["next"];
         });
     }
     $.ajaxSetup({async: true});
     return _options;
 }
 
-
 function changeVersion(defaultValue)
 {
     defaultValue = defaultValue || "";
-    $("#id_store").html(getoptions("/api/v1/token/stores/?version__id="+$("#id_version").val(), "id", "name"));
+    $("#id_store").html(getOptions(
+        "/api/v1/token/stores/?version__id=" + $("#id_version").val(),
+        "id",
+        "name"
+    ));
     $("#id_store").attr("disabled", false);
     if (typeof defaultValue === "string" && defaultValue !== "")
     {
@@ -28,9 +31,7 @@ function changeVersion(defaultValue)
     $("#id_version").attr("selected", "selected");
 }
 
-
 $(function() {
-        changeVersion($("#id_store").val());
-        $("#id_version").change(changeVersion);
-    }
-);
+    changeVersion($("#id_store").val());
+    $("#id_version").change(changeVersion);
+});
