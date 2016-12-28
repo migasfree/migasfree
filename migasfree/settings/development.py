f36cbe2f34b09c3cc81850696e9186835075cb0a
Jose Antonio Chavarría <jachavar@gmail.com>
2016-12-28 09:58:47 +0100

    jQuery is already loaded on project

--- migasfree/settings/development.py
+++ migasfree/settings/development.py
@@ -36,3 +36,7 @@ INSTALLED_APPS += ("debug_toolbar", 'django_extensions')
 INTERNAL_IPS = ("127.0.0.1",)
 
 MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': ''
+}
