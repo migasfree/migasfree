39d8c19bfac4fee48a20be99354286215dec0d3d
Jose Antonio Chavarría <jachavar@gmail.com>
2017-03-30 12:30:34 +0200

    Added Event abstract class (issue #208)

---
+++ migasfree/server/models/event.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016-2017 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2016-2017 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from django.db import models
+from django.db.models.aggregates import Count
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
+
+from .computer import Computer
+
+
+@python_2_unicode_compatible
+class Event(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('date')
+    )
+
+    computer = models.ForeignKey(
+        Computer,
+        verbose_name=_("computer")
+    )
+
+    @classmethod
+    def by_day(cls, computer_id, start_date, end_date):
+        return cls.objects.filter(
+             computer__id=computer_id,
+             created_at__range=(start_date, end_date)
+        ).extra(
+            {"day": "date_trunc('day', created_at)"}
+        ).values('day').annotate(count=Count('id')).order_by('-day')
+
+    def __str__(self):
+        return u'{} ({:%Y-%m-%d %H:%M:%S})'.format(self.computer, self.created_at)
+
+    class Meta:
+        abstract = True
