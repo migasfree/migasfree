2b92cbad2f42567cdb2997157cfa3691b60af204
Jose Antonio Chavarría <jachavar@gmail.com>
2017-03-30 12:31:59 +0200

    Changed name to Synchronization (issue #208)
    Inherits from Event class

--- migasfree/server/models/update.py
+++ migasfree/server/models/update.py
@@ -1,33 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.db.models.aggregates import Count
-from django.utils import timezone, dateformat
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-from . import Computer, Version, User, MigasLink
+from . import Version, MigasLink
+from .event import Event
+from .user import User
 
 
-class UpdateManager(models.Manager):
+class SynchronizationManager(models.Manager):
     def create(self, computer):
-        obj = Update()
+        obj = Synchronization()
         obj.computer = computer
         obj.version = computer.version
         obj.user = computer.sync_user
-        obj.date = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
         obj.save()
 
         return obj
 
 
-@python_2_unicode_compatible
-class Update(models.Model, MigasLink):
-    computer = models.ForeignKey(
-        Computer,
-        verbose_name=_("computer")
-    )
-
+class Synchronization(Event, MigasLink):
     user = models.ForeignKey(
         User,
         verbose_name=_("user")
@@ -39,33 +31,16 @@ class Update(models.Model, MigasLink):
         null=True
     )
 
-    date = models.DateTimeField(
-        verbose_name=_("date"),
-        default=0
-    )
-
-    objects = UpdateManager()
+    objects = SynchronizationManager()
 
     def save(self, *args, **kwargs):
-        super(Update, self).save(*args, **kwargs)
+        super(Synchronization, self).save(*args, **kwargs)
 
-        self.computer.sync_end_date = self.date
+        self.computer.sync_end_date = self.created_at
         self.computer.save()
-
-    @classmethod
-    def by_day(cls, computer_id, start_date, end_date):
-        return cls.objects.filter(
-            computer__id=computer_id,
-            date__range=(start_date, end_date)
-        ).extra(
-            {"day": "date_trunc('day', date)"}
-        ).values("day").annotate(count=Count("id")).order_by('-day')
-
-    def __str__(self):
-        return u'{} ({})'.format(self.computer, self.date)
 
     class Meta:
         app_label = 'server'
-        verbose_name = _("Update")
-        verbose_name_plural = _("Updates")
-        permissions = (("can_save_update", "Can save Update"),)
+        verbose_name = _("Synchronization")
+        verbose_name_plural = _("Synchronizations")
+        permissions = (("can_save_synchronization", "Can save Synchronization"),)
