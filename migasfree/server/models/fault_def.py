0d0b934349ef992a6b9413dd579f738a36c2a146
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-03 13:46:22 +0200

    Added excluded_attributes field to FaultDefinition (issue #210)

--- migasfree/server/models/fault_def.py
+++ migasfree/server/models/fault_def.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from django.db.models import Q
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-from . import Attribute, LANGUAGES_CHOICES, UserProfile, MigasLink
+from . import Attribute, UserProfile, MigasLink
 
 
 @python_2_unicode_compatible
-class FaultDef(models.Model, MigasLink):
+class FaultDefinition(models.Model, MigasLink):
     name = models.CharField(
         verbose_name=_("name"),
         max_length=50,
@@ -21,27 +23,33 @@ class FaultDef(models.Model, MigasLink):
         blank=True
     )
 
-    active = models.BooleanField(
-        verbose_name=_("active"),
+    enabled = models.BooleanField(
+        verbose_name=_("enabled"),
         default=True
     )
 
     language = models.IntegerField(
         verbose_name=_("programming language"),
-        default=0,
-        choices=LANGUAGES_CHOICES
+        default=settings.MIGASFREE_PROGRAMMING_LANGUAGES[0][0],
+        choices=settings.MIGASFREE_PROGRAMMING_LANGUAGES
     )
 
     code = models.TextField(
-        verbose_name=_("Code"),
-        null=False,
+        verbose_name=_("code"),
         blank=True
     )
 
-    attributes = models.ManyToManyField(
+    included_attributes = models.ManyToManyField(
         Attribute,
         blank=True,
-        verbose_name=_("attributes")
+        verbose_name=_("included attributes")
+    )
+
+    excluded_attributes = models.ManyToManyField(
+        Attribute,
+        related_name="ExcludeAttributeFaultDefinition",
+        blank=True,
+        verbose_name=_("excluded attributes")
     )
 
     users = models.ManyToManyField(
@@ -50,25 +58,40 @@ class FaultDef(models.Model, MigasLink):
         verbose_name=_("users")
     )
 
-    def save(self, *args, **kwargs):
-        self.code = self.code.replace("\r\n", "\n")
-        self.name = self.name.replace(" ", "_")
-        super(FaultDef, self).save(*args, **kwargs)
+    def list_included_attributes(self):
+        return self.included_attributes.all().values_list('value', flat=True)
 
-    def __str__(self):
-        return self.name
+    list_included_attributes.short_description = _("included attributes")
+
+    def list_excluded_attributes(self):
+        return self.excluded_attributes.all().values_list('value', flat=True)
+
+    list_excluded_attributes.short_description = _("excluded attributes")
+
+    def list_users(self):
+        return self.users.all().values_list('username', flat=True)
+
+    list_users.short_description = _("users")
 
     @staticmethod
     def enabled_for_attributes(attributes):
-        return FaultDef.objects.filter(
-            models.Q(active=True) &
-            models.Q(attributes__id__in=attributes)
-        ).distinct().values('language', 'name', 'code')
-        # NOTE .distinct('id') NOT supported in sqlite
+        return list(FaultDefinition.objects.filter(
+            Q(enabled=True) &
+            Q(included_attributes__id__in=attributes) &
+            ~Q(excluded_attributes__id__in=attributes)
+        ).distinct().values('language', 'name', 'code'))
+
+    def save(self, *args, **kwargs):
+        self.code = self.code.replace("\r\n", "\n")
+        self.name = self.name.replace(" ", "_")
+        super(FaultDefinition, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         app_label = 'server'
         verbose_name = _("Fault Definition")
         verbose_name_plural = _("Fault Definitions")
-        permissions = (("can_save_faultdef", "Can save Fault Definition"),)
+        permissions = (("can_save_faultdefinition", "Can save Fault Definition"),)
         ordering = ['name']
