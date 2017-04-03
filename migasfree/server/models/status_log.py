83115cf80d9411be646b00ac07e0ccc303e506ad
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-03 08:53:45 +0200

    Inherits from Event class

--- migasfree/server/models/status_log.py
+++ migasfree/server/models/status_log.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 *-*
 
 from django.db import models
-from django.db.models.aggregates import Count
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .computer import Computer
+from .event import Event
 
 
 class StatusLogManager(models.Manager):
@@ -18,13 +17,7 @@ class StatusLogManager(models.Manager):
         return obj
 
 
-@python_2_unicode_compatible
-class StatusLog(models.Model):
-    computer = models.ForeignKey(
-        Computer,
-        verbose_name=_("computer"),
-    )
-
+class StatusLog(Event):
     status = models.CharField(
         verbose_name=_('status'),
         max_length=20,
@@ -33,24 +26,7 @@ class StatusLog(models.Model):
         default='intended'
     )
 
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name=_('date')
-    )
-
     objects = StatusLogManager()
-
-    @classmethod
-    def by_day(cls, computer_id, start_date, end_date):
-        return cls.objects.filter(
-            computer__id=computer_id,
-            created_at__range=(start_date, end_date)
-        ).extra(
-            {"day": "date_trunc('day', created_at)"}
-        ).values("day").annotate(count=Count("id")).order_by('-day')
-
-    def __str__(self):
-        return '{} ({})'.format(self.computer, self.status)
 
     class Meta:
         app_label = 'server'
