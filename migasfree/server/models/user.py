d26a78cea63fc36ea2f9cdeba110c3e7d653cdec
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-04 11:08:50 +0200

    Unique together name and fullname

--- migasfree/server/models/user.py
+++ migasfree/server/models/user.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
 from . import MigasLink
 
@@ -22,21 +22,26 @@ class User(models.Model, MigasLink):
     name = models.CharField(
         verbose_name=_("name"),
         max_length=50,
-        unique=True
+        null=False
     )
 
     fullname = models.CharField(
         verbose_name=_("fullname"),
-        max_length=100
+        max_length=100,
+        blank=True
     )
 
     objects = UserManager()
 
     def __str__(self):
-        return u'%s (%s)' % (self.name, self.fullname.strip())
+        if self.fullname != '':
+            return u'{} ({})'.format(self.name, self.fullname.strip())
+
+        return self.name
 
     class Meta:
         app_label = 'server'
         verbose_name = _("User")
         verbose_name_plural = _("Users")
+        unique_together = (('name', 'fullname'),)
         permissions = (("can_save_user", "Can save User"),)
