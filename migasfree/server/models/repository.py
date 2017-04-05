8f75fc0a53dc51d45aec1a6b5fce5d69319e7561
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-05 14:43:56 +0200

    Changed Repository by Deployment (issue #211)

--- migasfree/server/models/repository.py
+++ migasfree/server/models/repository.py
@@ -1,85 +1,51 @@
 # -*- coding: utf-8 -*-
 
 import os
-import datetime
 import shutil
+import datetime
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
-from django.db.models.signals import pre_delete
+from django.db.models import Q
+from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
+from django.core.exceptions import ValidationError
 
-from ..functions import horizon
+from ..functions import time_horizon
 
 from . import (
     Version,
     Package,
     Attribute,
     Schedule,
+    ScheduleDelay,
     MigasLink
 )
 
 
-class RepositoryManager(models.Manager):
+class DeploymentManager(models.Manager):
     def by_version(self, version_id):
         return self.get_queryset().filter(version__id=version_id)
 
 
 @python_2_unicode_compatible
-class Repository(models.Model, MigasLink):
-    name = models.CharField(
-        verbose_name=_("name"),
-        max_length=50
-    )
-
-    version = models.ForeignKey(
-        Version,
-        verbose_name=_("version")
-    )
-
-    packages = models.ManyToManyField(
-        Package,
-        blank=True,
-        verbose_name=_("Packages/Set"),
-        help_text=_("Assigned Packages")
-    )
-
-    attributes = models.ManyToManyField(
-        Attribute,
-        blank=True,
-        verbose_name=_("attributes"),
-        help_text=_("Assigned Attributes")
-    )
-
-    excludes = models.ManyToManyField(
-        Attribute,
-        related_name="ExcludeAttribute",
-        blank=True,
-        verbose_name=_("excludes"),
-        help_text=_("Excluded Attributes")
-    )
-
-    schedule = models.ForeignKey(
-        Schedule,
-        null=True,
-        blank=True,
-        verbose_name=_("schedule")
-    )
-
-    active = models.BooleanField(
-        verbose_name=_("active"),
+class Deployment(models.Model, MigasLink):
+    enabled = models.BooleanField(
+        verbose_name=_('enabled'),
         default=True,
-        help_text=_("if you uncheck this field, the repository is hidden for"
-                    " all computers.")
+        help_text=_("if you uncheck this field, deployment is disabled for all"
+                    " computers.")
     )
 
-    date = models.DateField(
-        verbose_name=_("date"),
-        help_text=_("Date initial for distribute.")
+    name = models.CharField(
+        max_length=50,
+        verbose_name=_('name')
     )
+
+    version = models.ForeignKey(Version, verbose_name=_("version"))
 
     comment = models.TextField(
         verbose_name=_("comment"),
@@ -87,108 +53,173 @@ class Repository(models.Model, MigasLink):
         blank=True
     )
 
-    toinstall = models.TextField(
+    available_packages = models.ManyToManyField(
+        Package,
+        blank=True,
+        verbose_name=_('available packages'),
+        help_text=_('If a computer has installed one of these packages it will'
+                    ' be updated')
+    )
+
+    packages_to_install = models.TextField(
         verbose_name=_("packages to install"),
         null=True,
-        blank=True
+        blank=True,
+        help_text=_('Mandatory packages to install each time')
     )
 
-    toremove = models.TextField(
+    packages_to_remove = models.TextField(
         verbose_name=_("packages to remove"),
         null=True,
-        blank=True
+        blank=True,
+        help_text=_('Mandatory packages to remove each time')
     )
 
-    defaultpreinclude = models.TextField(
-        verbose_name=_("default preinclude packages"),
+    included_attributes = models.ManyToManyField(
+        Attribute,
+        blank=True,
+        verbose_name=_("included attributes")
+    )
+
+    excluded_attributes = models.ManyToManyField(
+        Attribute,
+        related_name="ExcludeAttribute",
+        blank=True,
+        verbose_name=_("excluded attributes")
+    )
+
+    schedule = models.ForeignKey(
+        Schedule,
+        verbose_name=_('schedule'),
         null=True,
         blank=True
     )
 
-    defaultinclude = models.TextField(
-        verbose_name=_("default include packages"),
+    start_date = models.DateField(
+        default=timezone.now,
+        verbose_name=_('start date')
+    )
+
+    default_preincluded_packages = models.TextField(
+        verbose_name=_("default pre-included packages"),
         null=True,
         blank=True
     )
 
-    defaultexclude = models.TextField(
-        verbose_name=_("default exclude packages"),
+    default_included_packages = models.TextField(
+        verbose_name=_("default included packages"),
         null=True,
         blank=True
     )
 
-    objects = RepositoryManager()
+    default_excluded_packages = models.TextField(
+        verbose_name=_("default excluded packages"),
+        null=True,
+        blank=True
+    )
 
-    def packages_link(self):
-        return ' '.join([pkg.link() for pkg in self.packages.all()])
-
-    packages_link.allow_tags = True
-    packages_link.short_description = _("Packages")
+    objects = DeploymentManager()
 
     def __str__(self):
         return self.name
 
+    @staticmethod
+    def get_percent(begin_date, end_date):
+        delta = end_date - begin_date
+        progress = datetime.datetime.now() - datetime.datetime.combine(
+            begin_date, datetime.datetime.min.time()
+        )
+
+        if delta.days > 0:
+            percent = float(progress.days) / delta.days * 100
+            if percent > 100:
+                percent = 100
+        else:
+            percent = 100
+
+        return percent
+
+    def schedule_timeline(self):
+        if self.schedule is None:
+            return None
+
+        delays = ScheduleDelay.objects.filter(
+            schedule__id=self.schedule.id
+        ).order_by('delay')
+
+        if len(delays) == 0:
+            return None
+
+        begin_date = time_horizon(self.start_date, delays[0].delay)
+        end_date = time_horizon(
+            self.start_date,
+            delays.reverse()[0].delay + delays.reverse()[0].duration
+        )
+
+        return {
+            'begin_date': str(begin_date),
+            'end_date': str(end_date),
+            'percent': '%d' % self.get_percent(begin_date, end_date)
+        }
+
     def save(self, *args, **kwargs):
         self.name = slugify(self.name)
 
-        # TODO Remove this condition and assign default value in toinstall field
-        if self.toinstall:
-            self.toinstall = self.toinstall.replace("\r\n", "\n")
+        if self.packages_to_install:
+            self.packages_to_install = self.packages_to_install.replace("\r\n", "\n")
+        if self.packages_to_remove:
+            self.packages_to_remove = self.packages_to_remove.replace("\r\n", "\n")
 
-        # TODO Remove this condition and assign default value in toremove field
-        if self.toremove:
-            self.toremove = self.toremove.replace("\r\n", "\n")
+        if self.default_preincluded_packages:
+            self.default_preincluded_packages = self.default_preincluded_packages.replace("\r\n", "\n")
+        if self.default_included_packages:
+            self.default_included_packages = self.default_included_packages.replace("\r\n", "\n")
+        if self.default_excluded_packages:
+            self.default_excluded_packages = self.default_excluded_packages.replace("\r\n", "\n")
 
-        super(Repository, self).save(*args, **kwargs)
+        super(Deployment, self).save(*args, **kwargs)
 
     @staticmethod
-    def available_repos(computer, attributes):
+    def available_deployments(computer, attributes):
         """
-        Return available repositories for a computer and attributes list
+        Return available deployments for a computer and attributes list
         """
-        # 1.- all repositories by attribute
-        attributed = Repository.objects.filter(
-            active=True
-        ).filter(
-            version__id=computer.version.id
-        ).filter(
-            attributes__id__in=attributes
-        ).filter(
-            date__lte=datetime.datetime.now().date()
+        # 1.- all deployments by attribute
+        attributed = Deployment.objects.filter(
+            version__id=computer.version.id,
+            enabled=True,
+            included_attributes__id__in=attributes,
+            start_date__lte=datetime.datetime.now().date()
         ).values_list('id', flat=True)
         lst = list(attributed)
 
-        # 2.- Add to "dic_repos" all repositories by schedule
-        scheduled = Repository.objects.filter(
-            active=True
-        ).filter(
-            version__id=computer.version.id
-        ).filter(
-            schedule__scheduledelay__attributes__id__in=attributes
+        # 2.- all deployments by schedule
+        scheduled = Deployment.objects.filter(
+            version__id=computer.version.id,
+            enabled=True,
+            schedule__delays__attributes__id__in=attributes
         ).extra(
             select={
-                "delay": "server_scheduledelay.delay",
-                "duration": "server_scheduledelay.duration"
+                'delay': 'server_scheduledelay.delay',
+                'duration': 'server_scheduledelay.duration'
             }
         )
 
-        for r in scheduled:
-            for duration in range(0, r.duration):
-                if computer.id % r.duration == duration:
-                    if horizon(
-                        r.date, r.delay + duration
+        for deploy in scheduled:
+            for duration in range(0, deploy.duration):
+                if computer.id % deploy.duration == duration:
+                    if time_horizon(
+                        deploy.start_date, deploy.delay + duration
                     ) <= datetime.datetime.now().date():
-                        lst.append(r.id)
+                        lst.append(deploy.id)
                         break
 
         # 3.- excluded attributes
-        repositories = Repository.objects.filter(
-            id__in=lst
-        ).filter(
-            ~models.Q(excludes__id__in=attributes)
+        deployments = Deployment.objects.filter(id__in=lst).filter(
+            ~Q(excluded_attributes__id__in=attributes)
         ).order_by('name')
 
-        return repositories
+        return deployments
 
     def path(self, name=None):
         return os.path.join(
@@ -199,14 +230,22 @@ class Repository(models.Model, MigasLink):
 
     class Meta:
         app_label = 'server'
-        verbose_name = _("Repository")
-        verbose_name_plural = _("Repositories")
-        unique_together = (("name", "version"),)
-        permissions = (("can_save_repository", "Can save Repository"),)
+        verbose_name = _('Deployment')
+        verbose_name_plural = _('Deployments')
+        unique_together = (('name', 'version'),)
+        permissions = (("can_save_deployment", "Can save Deployment"),)
         ordering = ['version__name', 'name']
 
 
-@receiver(pre_delete, sender=Repository)
+@receiver(pre_save, sender=Deployment)
+def pre_save_deployment(sender, instance, **kwargs):
+    if instance.id:
+        old_obj = Deployment.objects.get(pk=instance.id)
+        if old_obj.version.id != instance.version.id:
+            raise ValidationError(_('Is not allowed change version'))
+
+
+@receiver(pre_delete, sender=Deployment)
 def pre_delete_deployment(sender, instance, **kwargs):
     path = instance.path()
     if os.path.exists(path):
