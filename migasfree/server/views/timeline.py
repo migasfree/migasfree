ceeae8f7f221a90e9b265495db4f9f2da9e46d41
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-05 14:46:17 +0200

    Changed Repository by Deployment (issue #211)

--- migasfree/server/views/timeline.py
+++ migasfree/server/views/timeline.py
@@ -6,56 +6,45 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
-from ..functions import time_horizon, percent_horizon
-from ..models import Repository, ScheduleDelay
+from ..functions import time_horizon
+from ..models import Deployment, ScheduleDelay
 
 
 @login_required
 def timeline(request):
-    repository = get_object_or_404(Repository, pk=request.GET.get('id'))
+    deploy = get_object_or_404(Deployment, pk=request.GET.get('id'))
 
-    if repository.schedule is None or repository.schedule.id is None:
+    if deploy.schedule is None:
         return _('Without schedule')
 
+    schedule_timeline = deploy.schedule_timeline()
+    if not schedule_timeline:
+        return _('%s (without delays)') % deploy.schedule
+
     delays = ScheduleDelay.objects.filter(
-        schedule__id=repository.schedule.id
+        schedule__id=deploy.schedule.id
     ).order_by('delay')
 
-    if len(delays) == 0:
-        return _('%s (without delays)') % repository.schedule
-
-    date_format = "%Y-%m-%d"
-    begin_date = datetime.datetime.strptime(
-        str(time_horizon(repository.date, delays[0].delay)),
-        date_format
-    )
-    end_date = datetime.datetime.strptime(
-        str(time_horizon(
-            repository.date,
-            delays.reverse()[0].delay + delays.reverse()[0].duration
-        )),
-        date_format
-    )
-
     timeline_delays = []
+    date_format = "%Y-%m-%d"
     for item in delays:
         hori = datetime.datetime.strptime(
-            str(time_horizon(repository.date, item.delay)),
+            str(time_horizon(deploy.start_date, item.delay)),
             date_format
         )
         horf = datetime.datetime.strptime(
-            str(time_horizon(repository.date, item.delay + item.duration)),
+            str(time_horizon(deploy.start_date, item.delay + item.duration)),
             date_format
         )
 
-        deploy = 'default'
+        result = 'default'
         if hori <= datetime.datetime.now():
-            deploy = 'success'
+            result = 'success'
 
         timeline_delays.append({
-            'deploy': deploy,
+            'deploy': result,
             'date': hori.strftime("%a-%b-%d"),
-            'percent': int(percent_horizon(hori, horf)),
+            'percent': int(Deployment.get_percent(hori, horf)),
             'attributes': item.attributes.values_list("value", flat=True)
         })
 
@@ -64,8 +53,8 @@ def timeline(request):
         'includes/deployment_timeline_detail.html',
         {
             'timeline': {
-                'percent': int(percent_horizon(begin_date, end_date)),
-                'schedule': repository.schedule,
+                'percent': schedule_timeline['percent'],
+                'schedule': deploy.schedule,
                 'delays': timeline_delays
             }
         }
