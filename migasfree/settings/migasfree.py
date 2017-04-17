812245e810f499410fc89bc700237aed5142e295
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-17 09:27:46 +0200

    Added new parameters: * MIGASFREE__SETTINGS_OVERRIDE * MIGASFREE_PUBLIC_KEY * MIGASFREE_PRIVATE_KEY * MIGASFREE_PACKAGER_PUB_KEY * MIGASFREE_PACKAGER_PRI_KEY

--- migasfree/settings/migasfree.py
+++ migasfree/settings/migasfree.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 
+"""
+Please, don't edit this file
+Override or include settings at MIGASFREE_SETTINGS_OVERRIDE file
+"""
+
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MIGASFREE_AUTOREGISTER = True
 
-MIGASFREE_COMPUTER_SEARCH_FIELDS = ('id', 'name', )
+MIGASFREE_COMPUTER_SEARCH_FIELDS = ('id', 'name')
 
 MIGASFREE_SECONDS_MESSAGE_ALERT = 1800
 MIGASFREE_ORGANIZATION = 'My Organization'
-MIGASFREE_HELP_DESK = "Put here how you want to be found"
+MIGASFREE_HELP_DESK = 'Put here how you want to be found'
 
+MIGASFREE_SETTINGS_OVERRIDE = '/etc/migasfree-server/settings.py'
 MIGASFREE_APP_DIR = BASE_DIR
 MIGASFREE_PROJECT_DIR = os.path.dirname(MIGASFREE_APP_DIR)
 MIGASFREE_TMP_DIR = '/tmp'
@@ -20,13 +26,13 @@ MIGASFREE_TMP_DIR = '/tmp'
 # Variables can be: {{computer.<FIELD>}} and {{<<PROPERTYPREFIX>>}}
 # Samples:
 #    MIGASFREE_REMOTE_ADMIN_LINK = "https://myserver/?computer={{computer.name}}&port={{PRT}}"
-#    MIGASFREE_REMOTE_ADMIN_LINK = "ssh://user@{{computer.ip}} vnc://{{computer.ip}}"
+#    MIGASFREE_REMOTE_ADMIN_LINK = "ssh://user@{{computer.ip_address}} vnc://{{computer.ip_address}}"
 MIGASFREE_REMOTE_ADMIN_LINK = ""
 
 MIGASFREE_INVALID_UUID = [
-    "03000200-0400-0500-0006-000700080008",  # ASROCK
-    "00000000-0000-0000-0000-000000000000",
-    "00000000-0000-0000-0000-FFFFFFFFFFFF"
+    '03000200-0400-0500-0006-000700080008',  # ASROCK
+    '00000000-0000-0000-0000-000000000000',
+    '00000000-0000-0000-0000-FFFFFFFFFFFF',
 ]
 
 # Notifications
@@ -48,6 +54,14 @@ MIGASFREE_PROGRAMMING_LANGUAGES = (
     (5, 'cmd'),
 )
 
-# DEFAULT COMPUTER STATUS
+# Server Keys
+MIGASFREE_PUBLIC_KEY = 'migasfree-server.pub'
+MIGASFREE_PRIVATE_KEY = 'migasfree-server.pri'
+
+# Packager Keys
+MIGASFREE_PACKAGER_PUB_KEY = 'migasfree-packager.pub'
+MIGASFREE_PACKAGER_PRI_KEY = 'migasfree-packager.pri'
+
+# Default Computer Status
 # Values: 'intended', 'reserved', 'unknown', 'in repair', 'available' or 'unsubscribed'
 MIGASFREE_DEFAULT_COMPUTER_STATUS = 'intended'
