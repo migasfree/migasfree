060948d7292a00233243b988c3fcb044d1be6a9a
Jose Antonio Chavarría <jachavar@gmail.com>
2017-06-14 09:04:53 +0200

    Bugfixed removed package (store and metadata deployments) (closes #224)

--- migasfree/server/models/package.py
+++ migasfree/server/models/package.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 
 from django.db import models
 from django.core.exceptions import ValidationError
@@ -8,6 +9,8 @@ from django.core.urlresolvers import reverse
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.conf import settings
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 from . import Project, Store, MigasLink
 
@@ -75,6 +78,17 @@ class Package(models.Model, MigasLink):
     def path(project_name, store_name, name):
         return os.path.join(Store.path(project_name, store_name), name)
 
+    @staticmethod
+    def delete_from_store(path):
+        if os.path.exists(path):
+            try:
+                if os.path.isfile(path):
+                    os.remove(path)
+                else:
+                    shutil.rmtree(path, ignore_errors=True)
+            except OSError:
+                pass
+
     def create_dir(self):
         path = self.path(self.project.name, self.store.name, self.name)
         if not os.path.exists(path):
@@ -90,8 +104,7 @@ class Package(models.Model, MigasLink):
             raise ValidationError(_('Store must belong to the project'))
 
         queryset = Package.objects.filter(
-            name=self.name
-        ).filter(
+            name=self.name,
             project__id=self.project.id
         ).filter(~models.Q(id=self.id))
         if queryset.exists():
@@ -110,3 +123,24 @@ class Package(models.Model, MigasLink):
         verbose_name_plural = _("Packages/Sets")
         unique_together = (("name", "project"),)
         permissions = (("can_save_package", "Can save Package"),)
+
+
+@receiver(pre_delete, sender=Package)
+def pre_delete_package(sender, instance, **kwargs):
+    from ..tasks import create_repository_metadata
+    from .deployment import Deployment
+
+    path = Package.path(
+        instance.project.name,
+        instance.store.name,
+        instance.name
+    )
+    Package.delete_from_store(path)
+
+    queryset = Deployment.objects.filter(
+        available_packages__in=[instance],
+        enabled=True
+    )
+    for deploy in queryset:
+        deploy.available_packages.remove(instance)
+        create_repository_metadata(deploy)
