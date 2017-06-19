18b4e62dd144c8c2920cba3984eb2f8411d0b180
Jose Antonio Chavarría <jachavar@gmail.com>
2017-06-19 09:04:38 +0200

    Changed import django.core.urlresolvers by django.urls (Django 1.10)

--- migasfree/server/models/store.py
+++ migasfree/server/models/store.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.utils.text import slugify
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.conf import settings
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
