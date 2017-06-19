a4691c2200e150d373908f20bd0bbc7e02ae1616
Jose Antonio Chavarría <jachavar@gmail.com>
2017-06-19 09:06:49 +0200

    Changed import django.core.urlresolvers by django.urls (Django 1.10)

--- migasfree/server/views/login.py
+++ migasfree/server/views/login.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext as _
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from ..models import Project, UserProfile
 
