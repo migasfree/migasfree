845b96c6ea99eb51cce6ce2c9fb2f539573614e8
Jose Antonio Chavarría <jachavar@gmail.com>
2017-06-19 08:57:09 +0200

    Upgraded Django Swagger and Django Debug Toolbar components

--- migasfree/urls.py
+++ migasfree/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 from ajax_select import urls as ajax_select_urls
 from rest_framework.authtoken import views
+from rest_framework_swagger.views import get_swagger_view
 
 from server.routers import router
 from catalog.routers import router as catalog_router
@@ -13,8 +14,12 @@ from catalog.routers import router as catalog_router
 from django.contrib import admin
 admin.autodiscover()
 
+swagger_schema_view = get_swagger_view(
+    title='Migasfree REST API',
+)
+
 urlpatterns = [
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^docs/', swagger_schema_view, name='docs'),
     url(r'^token-auth/$', views.obtain_auth_token),
     url(r'^api/v1/token/', include(router.urls)),
     url(r'^api/v1/public/catalog/', include(catalog_router.urls)),
@@ -28,15 +33,21 @@ urlpatterns = [
     url(r'^markdownx/', include('markdownx.urls')),
 ]
 
-if settings.DEBUG and settings.STATIC_ROOT is not None:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
 
-if settings.DEBUG and settings.MEDIA_ROOT is not None:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-        show_indexes=True
-    )
+    if settings.STATIC_ROOT is not None:
+        urlpatterns += static(
+            settings.STATIC_URL,
+            document_root=settings.STATIC_ROOT
+        )
+
+    if settings.MEDIA_ROOT is not None:
+        urlpatterns += static(
+            settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT,
+            show_indexes=True
+        )
