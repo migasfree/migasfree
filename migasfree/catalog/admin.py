ebb92358245bb2e90aa83fa30aac015e1758f6e8
Jose Antonio Chavarría <jachavar@gmail.com>
2017-06-29 12:59:45 +0200

    Added PackagesByProject model (issue #225)

--- migasfree/catalog/admin.py
+++ migasfree/catalog/admin.py
@@ -2,11 +2,55 @@
 
 from django.contrib import admin
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-from .models import Application
+from ajax_select import make_ajax_form
 from form_utils.widgets import ImageWidget
 
 from migasfree.server.admin.migasfree import MigasAdmin, MigasFields
+
+from .models import Application, PackagesByProject
+
+
+@admin.register(PackagesByProject)
+class PackagesByProjectAdmin(MigasAdmin):
+    form = make_ajax_form(
+        PackagesByProject,
+        {'project': 'project'}
+    )
+
+    list_display = (
+        'id', 'application_link', 'project_link', 'packages_to_install'
+    )
+    list_display_links = ('id',)
+    list_filter = ('application__name',)
+    search_fields = ('application__name', 'packages_to_install')
+
+    application_link = MigasFields.link(model=PackagesByProject, name='application')
+    project_link = MigasFields.objects_link(
+        model=PackagesByProject, name='project',
+        description=_('project')
+    )
+
+    def get_queryset(self, request):
+        return super(PackagesByProjectAdmin, self).get_queryset(
+            request
+        ).prefetch_related('project')
+
+
+class PackagesByProjectLine(admin.TabularInline):
+    model = PackagesByProject
+    fields = ('project', 'packages_to_install')
+    ordering = ('project',)
+    extra = 0
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super(PackagesByProjectLine, self).get_formset(request, obj, **kwargs)
+        form = formset.form
+        form.base_fields['project'].widget.can_change_related = False
+        form.base_fields['project'].widget.can_add_related = False
+
+        return formset
 
 
 @admin.register(Application)
@@ -14,15 +58,18 @@ class ApplicationAdmin(MigasAdmin):
     formfield_overrides = {
         models.ImageField: {'widget': ImageWidget}
     }
-    list_display = ('name', 'project_link', 'score', 'level', 'category',)
+    list_display = ('name', 'score', 'level', 'category',)
     list_display_links = ('name',)
-    list_filter = ('project', 'level', 'category')
+    list_filter = ('level', 'category')
     ordering = ('name',)
     fields = (
-        'name', 'project', 'category', 'level', 'score', 'icon',
-        'description', 'packages_to_install'
+        'name', 'category', 'level',
+        'score', 'icon', 'description'
     )
     search_fields = ('name', 'description')
+
+    inlines = [PackagesByProjectLine]
+    extra = 0
 
     project_link = MigasFields.link(
         model=Application, name='project', order='project__name'
