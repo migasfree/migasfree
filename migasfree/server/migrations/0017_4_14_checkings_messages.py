7807d72598e9a232d75590a3724abdb32e6a914c
Jose Antonio Chavarría <jachavar@gmail.com>
2017-07-05 13:44:50 +0200

    Removed Checking model (issue #230)

--- migasfree/server/migrations/0017_4_14_checkings_messages.py
+++ migasfree/server/migrations/0017_4_14_checkings_messages.py
@@ -14,19 +14,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RenameField(
-            model_name='Checking',
-            old_name='active',
-            new_name='enabled',
-        ),
-        migrations.AlterField(
-            model_name='Checking',
-            name='enabled',
-            field=models.BooleanField(
-                default=True,
-                verbose_name='enabled'
-            ),
-        ),
-        migrations.RenameField(
             model_name='Message',
             old_name='date',
             new_name='updated_at',
@@ -41,43 +28,10 @@ class Migration(migrations.Migration):
             ),
             preserve_default=False,
         ),
-        migrations.RunSQL(
-            [(
-                "UPDATE server_checking SET code=%s WHERE id=5;",
-                [
-                    "from django.conf import settings\nfrom datetime import datetime, timedelta\nfrom migasfree.server.models import Message\nurl = '/computer_messages/'\nmsg = 'Computer updating now'\nt = datetime.now() - timedelta(0, settings.MIGASFREE_SECONDS_MESSAGE_ALERT)\nresult = Message.objects.filter(updated_at__gt=t).count()\nalert = 'info'\ntarget = 'computer'\n"
-                ]
-            )],
-            [(
-                "UPDATE server_checking SET code=%s WHERE id=5;",
-                [
-                    "from django.conf import settings\nfrom datetime import datetime, timedelta\nfrom migasfree.server.models import Message\nurl = '/computer_messages/'\nmsg = 'Computer updating now'\nt = datetime.now() - timedelta(0, settings.MIGASFREE_SECONDS_MESSAGE_ALERT)\nresult = Message.objects.filter(date__gt=t).count()\nalert = 'info'\ntarget = 'computer'\n"
-                ]
-            )]
-        ),
-        migrations.RunSQL(
-            [(
-                "UPDATE server_checking SET code=%s WHERE id=6;",
-                [
-                    "from django.conf import settings\nfrom datetime import datetime, timedelta\nfrom migasfree.server.models import Message\nurl = '/computer_messages/'\nmsg = 'Computer delayed' \nt = datetime.now() - timedelta(0, settings.MIGASFREE_SECONDS_MESSAGE_ALERT)\nresult = Message.objects.filter(updated_at__lt=t).count()\nalert = 'warning'\ntarget = 'computer'\n"
-                ]
-            )],
-            [(
-                "UPDATE server_checking SET code=%s WHERE id=6;",
-                [
-                    "from django.conf import settings\nfrom datetime import datetime, timedelta\nfrom migasfree.server.models import Message\nurl = '/computer_messages/'\nmsg = 'Computer delayed' \nt = datetime.now() - timedelta(0, settings.MIGASFREE_SECONDS_MESSAGE_ALERT)\nresult = Message.objects.filter(date__lt=t).count()\nalert = 'warning'\ntarget = 'computer'\n"
-                ]
-            )]
-        ),
         migrations.DeleteModel(
             name='MessageServer',
         ),
-        migrations.RunSQL(
-            "DELETE FROM server_checking WHERE id=7;",
-            migrations.RunSQL.noop
-        ),
-        migrations.RunSQL(
-            "DELETE FROM server_checking WHERE id=8;",
-            migrations.RunSQL.noop
+        migrations.DeleteModel(
+            name='Checking',
         ),
     ]
