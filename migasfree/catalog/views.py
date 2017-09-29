fe82b1a87e769d3c0a4ab8cb0bb6ed29d55b85a7
Jose Antonio Chavarría <jachavar@gmail.com>
2017-09-29 13:58:06 +0200

    Added categories and levels endpoints

--- migasfree/catalog/views.py
+++ migasfree/catalog/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from rest_framework import viewsets, mixins, filters
+from rest_framework import viewsets, mixins, filters, status
 from rest_framework_filters import backends
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 from migasfree.server.permissions import PublicPermission
 from . import models, serializers
@@ -16,3 +18,17 @@ class ApplicationViewSet(
     filter_class = ApplicationFilter
     filter_backends = (filters.OrderingFilter, backends.DjangoFilterBackend)
     permission_classes = (PublicPermission,)
+
+    @list_route(methods=['get'])
+    def levels(self, request):
+        return Response(
+            dict(models.Application.LEVELS),
+            status=status.HTTP_200_OK
+        )
+
+    @list_route(methods=['get'])
+    def categories(self, request):
+        return Response(
+            dict(models.Application.CATEGORIES),
+            status=status.HTTP_200_OK
+        )
