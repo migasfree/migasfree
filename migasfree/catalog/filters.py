421ef57ebea05283b6106dbdaf8cf3db4640426d
Jose Antonio Chavarría <jachavar@gmail.com>
2017-09-29 14:49:45 +0200

    Added description filter

--- migasfree/catalog/filters.py
+++ migasfree/catalog/filters.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import django_filters
+
 from rest_framework import filters
 
 from .models import Application
 
 
 class ApplicationFilter(filters.FilterSet):
+    description = django_filters.CharFilter(
+        name='description', lookup_expr='icontains'
+    )
+
     class Meta:
         model = Application
         fields = ['level', 'category', 'packages_by_project__project__name']
