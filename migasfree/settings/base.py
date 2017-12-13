9c7351ea598f5b51bc718820197a345733ec1b13
Jose Antonio Chavarría <jachavar@gmail.com>
2017-12-13 13:38:57 +0100

    Added GZipMiddleware

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'migasfree.middleware.threadlocals.ThreadLocalMiddleware',
 ]
 
