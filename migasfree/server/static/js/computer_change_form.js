820b9994333ed4cbb229b0843507a06772e221de
Jose Antonio Chavarría <jachavar@gmail.com>
2018-01-03 14:39:07 +0100

    Changed alert by console.log

--- migasfree/server/static/js/computer_change_form.js
+++ migasfree/server/static/js/computer_change_form.js
@@ -21,19 +21,19 @@ $(function(){
         url: "/api/v1/token/computers/" + $("input#computer-id").val() + "/",
         error: function( jqXHR, textStatus, errorThrown ) {
             if (jqXHR.status === 0) {
-                alert('Not connect: Verify Network.');
+                console.log('Not connect: Verify Network.');
             } else if (jqXHR.status == 404) {
-                alert('Requested page not found [404]');
+                console.log('Requested page not found [404]');
             } else if (jqXHR.status == 500) {
-                alert('Internal Server Error [500].');
+                console.log('Internal Server Error [500].');
             } else if (textStatus === 'parsererror') {
-                alert('Requested JSON parse failed.');
+                console.log('Requested JSON parse failed.');
             } else if (textStatus === 'timeout') {
-                alert('Time out error.');
+                console.log('Time out error.');
             } else if (textStatus === 'abort') {
-                alert('Ajax request aborted.');
+                console.log('Ajax request aborted.');
             } else {
-                alert('Uncaught Error: ' + jqXHR.responseText);
+                console.log('Uncaught Error: ' + jqXHR.responseText);
             }
         }
     });
