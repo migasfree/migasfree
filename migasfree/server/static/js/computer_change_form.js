085eff2d559efd2dc287c23b56ff816e321f4032
Jose Antonio Chavarría <jachavar@gmail.com>
2018-01-11 17:35:18 +0100

    JSLint cleanup

--- migasfree/server/static/js/computer_change_form.js
+++ migasfree/server/static/js/computer_change_form.js
@@ -21,19 +21,19 @@ $(function(){
         url: "/api/v1/token/computers/" + $("input#computer-id").val() + "/",
         error: function( jqXHR, textStatus, errorThrown ) {
             if (jqXHR.status === 0) {
-                console.log('Not connect: Verify Network.');
-            } else if (jqXHR.status == 404) {
-                console.log('Requested page not found [404]');
-            } else if (jqXHR.status == 500) {
-                console.log('Internal Server Error [500].');
-            } else if (textStatus === 'parsererror') {
-                console.log('Requested JSON parse failed.');
-            } else if (textStatus === 'timeout') {
-                console.log('Time out error.');
-            } else if (textStatus === 'abort') {
-                console.log('Ajax request aborted.');
+                console.log("Not connect: Verify Network.");
+            } else if (jqXHR.status === 404) {
+                console.log("Requested page not found [404]");
+            } else if (jqXHR.status === 500) {
+                console.log("Internal Server Error [500].");
+            } else if (textStatus === "parsererror") {
+                console.log("Requested JSON parse failed.");
+            } else if (textStatus === "timeout") {
+                console.log("Time out error.");
+            } else if (textStatus === "abort") {
+                console.log("Ajax request aborted.");
             } else {
-                console.log('Uncaught Error: ' + jqXHR.responseText);
+                console.log("Uncaught Error: " + jqXHR.responseText);
             }
         }
     });
