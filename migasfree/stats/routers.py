3b5e74a4d5efeeac4d7e8198a0b89def69521b17
Jose Antonio Chavarría <jachavar@gmail.com>
2018-04-26 08:00:18 +0200

    Fixed import (issue #255)

--- migasfree/stats/routers.py
+++ migasfree/stats/routers.py
@@ -2,8 +2,8 @@
 
 from rest_framework import routers
 
-from . import views
+from .views import SyncStatsViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r'stats/syncs', views.SyncStatsViewSet, base_name='stats-syncs')
+router.register(r'stats/syncs', SyncStatsViewSet, base_name='stats-syncs')
