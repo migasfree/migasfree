130bbcc69e88d67f556e5f425ae0005cc05af88b
Jose Antonio Chavarría <jachavar@gmail.com>
2018-11-07 12:13:46 +0100

    Splitted liberation_summary in several views (issue #270)

--- migasfree/stats/views/software.py
+++ migasfree/stats/views/software.py
@@ -5,12 +5,12 @@ import json
 from collections import defaultdict
 
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count, Q
+from django.db.models import Count
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils.translation import ugettext as _
 
-from migasfree.server.models import Deployment, Project, Store, Package
+from migasfree.server.models import Project, Store, Package
 from migasfree.catalog.models import Application
 
 
@@ -26,14 +26,14 @@ def application_by_category():
     ).annotate(
         count=Count('category')
     ).order_by('-count'):
-        percent = float(item['count']) / total * 100
+        percent = float(item.get('count')) / total * 100
         data.append({
-            'name': u'{}'.format(dict(Application.CATEGORIES)[item['category']]),
-            'value': item['count'],
+            'name': u'{}'.format(dict(Application.CATEGORIES)[item.get('category')]),
+            'value': item.get('count'),
             'y': float('{:.2f}'.format(percent)),
             'url': link.replace(
                 '_REPLACE_',
-                'category__exact={}'.format(item['category'])
+                'category__exact={}'.format(item.get('category'))
             ),
         })
 
@@ -57,14 +57,14 @@ def application_by_level():
     ).annotate(
         count=Count('level')
     ).order_by('-count'):
-        percent = float(item['count']) / total * 100
+        percent = float(item.get('count')) / total * 100
         data.append({
-            'name': u'{}'.format(dict(Application.LEVELS)[item['level']]),
-            'value': item['count'],
+            'name': u'{}'.format(dict(Application.LEVELS)[item.get('level')]),
+            'value': item.get('count'),
             'y': float('{:.2f}'.format(percent)),
             'url': link.replace(
                 '_REPLACE_',
-                'level__exact={}'.format(item['level'])
+                'level__exact={}'.format(item.get('level'))
             ),
         })
 
@@ -107,7 +107,7 @@ def package_by_store(user):
     data = []
     for project in Project.objects.scope(user).all():
         if project.id in values:
-            count = sum(item['value'] for item in values[project.id])
+            count = sum(item.get('value') for item in values[project.id])
             percent = float(count) / total * 100
             data.append(
                 {
@@ -143,14 +143,14 @@ def store_by_project(user):
     ).annotate(
         count=Count('id')
     ).order_by('-count'):
-        percent = float(item['count']) / total * 100
+        percent = float(item.get('count')) / total * 100
         data.append({
-            'name': item['project__name'],
-            'value': item['count'],
+            'name': item.get('project__name'),
+            'value': item.get('count'),
             'y': float('{:.2f}'.format(percent)),
             'url': link.replace(
                 '_REPLACE_',
-                'project__id__exact={}'.format(item['project__id'])
+                'project__id__exact={}'.format(item.get('project__id'))
             ),
         })
 
@@ -162,203 +162,53 @@ def store_by_project(user):
     }
 
 
-def deployment_by_enabled(user):
-    total = Deployment.objects.scope(user).count()
-    link = '{}?_REPLACE_'.format(
-        reverse('admin:server_deployment_changelist')
-    )
-
-    values_null = defaultdict(list)
-    for item in Deployment.objects.scope(user).filter(
-        enabled=True
-    ).values(
-        'project__id',
-    ).annotate(
-        count=Count('id')
-    ).order_by('project__id', '-count'):
-        percent = float(item.get('count')) / total * 100
-        values_null[item.get('project__id')].append(
-            {
-                'name': _('Enabled'),
-                'value': item.get('count'),
-                'y': float('{:.2f}'.format(percent)),
-                'url': link.replace(
-                    '_REPLACE_',
-                    'project__id__exact={}&enabled__exact=1'.format(
-                        item.get('project__id')
-                    )
-                ),
-            }
-        )
-
-    values_not_null = defaultdict(list)
-    for item in Deployment.objects.scope(user).filter(
-        enabled=False,
-    ).values(
-        'project__id',
-    ).annotate(
-        count=Count('id')
-    ).order_by('project__id', '-count'):
-        percent = float(item.get('count')) / total * 100
-        values_not_null[item.get('project__id')].append(
-            {
-                'name': _('Disabled'),
-                'value': item.get('count'),
-                'y': float('{:.2f}'.format(percent)),
-                'url': link.replace(
-                    '_REPLACE_',
-                    'project__id__exact={}&enabled__exact=0'.format(
-                        item.get('project__id')
-                    )
-                ),
-            }
-        )
-
-    data = []
-    for project in Project.objects.scope(user).all():
-        count = 0
-        data_project = []
-        if project.id in values_null:
-            count += values_null[project.id][0]['value']
-            data_project.append(values_null[project.id][0])
-        if project.id in values_not_null:
-            count += values_not_null[project.id][0]['value']
-            data_project.append(values_not_null[project.id][0])
-        if count:
-            percent = float(count) / total * 100
-            data.append(
-                {
-                    'name': project.name,
-                    'value': count,
-                    'y': float('{:.2f}'.format(percent)),
-                    'url': link.replace(
-                        '_REPLACE_',
-                        'project__id__exact={}'.format(project.id)
-                    ),
-                    'data': data_project
-                }
-            )
-
-    return {
-        'title': _('Deployments / Enabled'),
-        'total': total,
-        'data': json.dumps(data),
-        'url': link.replace('?_REPLACE_', ''),
-    }
-
-
-def deployment_by_schedule(user):
-    total = Deployment.objects.scope(user).count()
-    link = '{}?_REPLACE_'.format(
-        reverse('admin:server_deployment_changelist')
-    )
-
-    values_null = defaultdict(list)
-    for item in Deployment.objects.scope(user).filter(
-        schedule=None
-    ).values(
-        'project__id',
-    ).annotate(
-        count=Count('id')
-    ).order_by('project__id', '-count'):
-        percent = float(item.get('count')) / total * 100
-        values_null[item.get('project__id')].append(
-            {
-                'name': _('Without schedule'),
-                'value': item.get('count'),
-                'y': float('{:.2f}'.format(percent)),
-                'url': link.replace(
-                    '_REPLACE_',
-                    'project__id__exact={}&schedule__isnull=True'.format(
-                        item.get('project__id')
-                    )
-                ),
-            }
-        )
-
-    values_not_null = defaultdict(list)
-    for item in Deployment.objects.scope(user).filter(
-        ~Q(schedule=None)
-    ).values(
-        'project__id',
-    ).annotate(
-        count=Count('id')
-    ).order_by('project__id', '-count'):
-        percent = float(item.get('count')) / total * 100
-        values_not_null[item.get('project__id')].append(
-            {
-                'name': _('With schedule'),
-                'value': item.get('count'),
-                'y': float('{:.2f}'.format(percent)),
-                'url': link.replace(
-                    '_REPLACE_',
-                    'project__id__exact={}&schedule__isnull=False'.format(
-                        item.get('project__id')
-                    )
-                ),
-            }
-        )
-
-    data = []
-    for project in Project.objects.scope(user).all():
-        count = 0
-        data_project = []
-        if project.id in values_null:
-            count += values_null[project.id][0]['value']
-            data_project.append(values_null[project.id][0])
-        if project.id in values_not_null:
-            count += values_not_null[project.id][0]['value']
-            data_project.append(values_not_null[project.id][0])
-        if count:
-            percent = float(count) / total * 100
-            data.append(
-                {
-                    'name': project.name,
-                    'value': count,
-                    'y': float('{:.2f}'.format(percent)),
-                    'url': link.replace(
-                        '_REPLACE_',
-                        'project__id__exact={}'.format(project.id)
-                    ),
-                    'data': data_project
-                }
-            )
-
-    return {
-        'title': _('Deployments / Schedule'),
-        'total': total,
-        'data': json.dumps(data),
-        'url': link.replace('?_REPLACE_', ''),
-    }
-
-
 @login_required
-def liberation_dashboard(request):
+def stores_summary(request):
     user = request.user.userprofile
 
     return render(
         request,
-        'software_dashboard.html',
+        'stores_summary.html',
         {
-            'title': _("Summary"),
+            'title': _("Stores"),
             'chart_options': {
                 'no_data': _('There are no data to show'),
                 'reset_zoom': _('Reset Zoom'),
-                'months': json.dumps([
-                    _('January'), _('February'), _('March'),
-                    _('April'), _('May'), _('June'),
-                    _('July'), _('August'), _('September'),
-                    _('October'), _('November'), _('December')
-                ]),
-                'weekdays': json.dumps([
-                    _('Sunday'), _('Monday'), _('Tuesday'), _('Wednesday'),
-                    _('Thursday'), _('Friday'), _('Saturday')
-                ]),
             },
-            'deployment_by_enabled': deployment_by_enabled(user),
-            'deployment_by_schedule': deployment_by_schedule(user),
             'store_by_project': store_by_project(user),
+        }
+    )
+
+
+@login_required
+def packages_summary(request):
+    user = request.user.userprofile
+
+    return render(
+        request,
+        'packages_summary.html',
+        {
+            'title': _("Packages/Sets"),
+            'chart_options': {
+                'no_data': _('There are no data to show'),
+                'reset_zoom': _('Reset Zoom'),
+            },
             'package_by_store': package_by_store(user),
+        }
+    )
+
+
+@login_required
+def applications_summary(request):
+    return render(
+        request,
+        'applications_summary.html',
+        {
+            'title': _("Applications"),
+            'chart_options': {
+                'no_data': _('There are no data to show'),
+                'reset_zoom': _('Reset Zoom'),
+            },
             'application_by_category': application_by_category(),
             'application_by_level': application_by_level(),
         }
