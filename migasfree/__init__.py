88c23f0f8596c0f470a4f97487f56173402cd8a6
Jose Antonio Chavarría <jachavar@gmail.com>
2018-12-11 13:52:20 +0100

    Bumped version to 4.17

--- migasfree/__init__.py
+++ migasfree/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-__version__ = "4.16"
+__version__ = "4.17"
 __author__ = [
     'Alberto Gacías <alberto@migasfree.org>',
     'Jose Antonio Chavarría <jachavar@gmail.com>'
