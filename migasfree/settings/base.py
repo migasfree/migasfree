4b6af5eebaf8b874a247d6f0a1c936987742c3a4
Jose Antonio Chavarría <jachavar@gmail.com>
2019-04-05 12:07:34 +0200

    Added django-cors-headers (closes #286)

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -50,6 +50,7 @@ ADMIN_SITE_ROOT_URL = '/admin/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,6 +116,7 @@ INSTALLED_APPS = (
     'rest_framework_swagger',
     'rest_auth',
     'django_filters',
+    'corsheaders',
     'form_utils',
     'markdownx',
     'datetimewidget',
