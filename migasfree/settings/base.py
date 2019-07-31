32255bcacab360f1d9964469e4d948daf8178ead
Jose Antonio Chavarría <jachavar@gmail.com>
2019-07-31 12:12:31 +0200

    Removed DATETIME_FORMAT to back to default value ('iso-8601')

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -138,7 +138,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-    'DATETIME_FORMAT': '%c',
 }
 
 SWAGGER_SETTINGS = {
