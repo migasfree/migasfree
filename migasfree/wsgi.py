53411c518f0173031746edc363067be662a7a77b
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-05 11:17:05 +0100

    Changed IO priority to idle (closes #299)

--- migasfree/wsgi.py
+++ migasfree/wsgi.py
@@ -1,5 +1,11 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migasfree.settings.production")
+import psutil
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migasfree.settings.production")
+
 application = get_wsgi_application()
+
+p = psutil.Process(os.getpid())
+p.ionice(psutil.IOPRIO_CLASS_IDLE)
