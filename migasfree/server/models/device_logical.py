cae993e6f98664fb0091817d9aff4a68a1b815fb
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-10 12:59:05 +0100

    Removed unused import

--- migasfree/server/models/device_logical.py
+++ migasfree/server/models/device_logical.py
@@ -2,8 +2,6 @@
 
 import json
 
-from past.builtins import basestring
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
