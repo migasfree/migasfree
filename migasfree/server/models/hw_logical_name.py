dafedb8fac31d0b9130d19c273eaa9e9fdbf8079
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-11 13:36:33 +0100

    Removed python_2_unicode_compatible (#300)

--- migasfree/server/models/hw_logical_name.py
+++ migasfree/server/models/hw_logical_name.py
@@ -2,7 +2,6 @@
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
 
 from . import HwNode
 
@@ -18,7 +17,6 @@ class HwLogicalNameManager(models.Manager):
         return obj
 
 
-@python_2_unicode_compatible
 class HwLogicalName(models.Model):
     node = models.ForeignKey(
         HwNode,
