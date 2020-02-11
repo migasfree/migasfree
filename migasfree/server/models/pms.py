2d358e4497e70d4155d2ad3a5c41ac1a06d5a2d8
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-11 13:35:43 +0100

    Removed python_2_unicode_compatible (#300)

--- migasfree/server/models/pms.py
+++ migasfree/server/models/pms.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from . import MigasLink
 
 
-@python_2_unicode_compatible
 class Pms(models.Model, MigasLink):
     """
     Package Management System
