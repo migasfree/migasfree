47b87e93a1fa6eaccad3d779e446c46a0643789a
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-11 13:34:49 +0100

    Removed python_2_unicode_compatible (#300)

--- migasfree/server/models/user.py
+++ migasfree/server/models/user.py
@@ -2,7 +2,6 @@
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
 
 from . import MigasLink
 
@@ -26,7 +25,6 @@ class UserManager(DomainUserManager):
         return obj
 
 
-@python_2_unicode_compatible
 class User(models.Model, MigasLink):
     name = models.CharField(
         verbose_name=_("name"),
