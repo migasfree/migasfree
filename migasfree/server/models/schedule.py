fd4978f9dcad0ee5c239ea4b76ba913e5874f99b
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-11 13:35:19 +0100

    Removed python_2_unicode_compatible (#300)

--- migasfree/server/models/schedule.py
+++ migasfree/server/models/schedule.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from . import MigasLink
 
 
-@python_2_unicode_compatible
 class Schedule(models.Model, MigasLink):
     name = models.CharField(
         verbose_name=_('name'),
