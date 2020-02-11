3dc69c82231fd7aee8e7fbd6db4b001866d28df3
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-11 13:35:25 +0100

    Removed python_2_unicode_compatible (#300)

--- migasfree/server/models/query.py
+++ migasfree/server/models/query.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
-@python_2_unicode_compatible
 class Query(models.Model):
     name = models.CharField(
         verbose_name=_("name"),
