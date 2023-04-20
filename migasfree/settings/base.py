101f8906773093ad20bd74693e3861933b39c7e1
Jose Antonio Chavarría <jachavar@gmail.com>
2023-04-20 14:44:09 +0200

    feat: upgraded django-filter and djangorestframework-filters components

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -135,7 +135,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_filters.backends.DjangoFilterBackend',
+        'rest_framework_filters.backends.RestFrameworkFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
