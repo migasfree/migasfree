dbb2cc44044d835fa5712a80b780ef737ab9a77f
jact <jachavar@gmail.com>
2025-09-05 08:56:09 +0200

    feat: bumped version to 4.22

--- migasfree/__init__.py
+++ migasfree/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-__version__ = "4.21"
+__version__ = "4.22"
 __author__ = [
     'Alberto Gacías <alberto@migasfree.org>',
     'Jose Antonio Chavarría <jachavar@gmail.com>'
